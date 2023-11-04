ca7b40fec1f6d16391ad3036c53df53de5bbf7df
Jørgen Vatle <jorgen@wcaservices.net>
2023-11-04 02:38:17 +0200

    Log full package name when versioning package for release.

--- .bin/release-meteor-package.mjs
+++ .bin/release-meteor-package.mjs
@@ -4,6 +4,7 @@ import FS from 'fs/promises';
 
 // Assuming this is launched from the repository root for now.
 const repoPath = process.cwd();
+const PACKAGE_NAME_REGEX = /name:\s*'(?<packageName>(?<author>[\w\-._]+):(?<name>[\w\-._]+))'\s*,/;
 const PACKAGE_VERSION_REGEX = /version:\s*'(?<version>[\d.]+)'\s*,/;
 const CHANGESET_STATUS_FILE = 'changeset-status.json';
 const meteorPackage = {
@@ -27,6 +28,7 @@ async function applyVersion() {
     console.log(`ℹ️  New version ${release.newVersion} for ${meteorPackage.releaseName} detected`);
 
     let packageJsContent = await FS.readFile(meteorPackage.packageJsPath, 'utf-8');
+    const packageName = packageJsContent.match(PACKAGE_NAME_REGEX)?.groups.packageName;
     const currentVersion = packageJsContent.match(PACKAGE_VERSION_REGEX)?.groups?.version
     if (!currentVersion) {
         throw new Error(`Unable to read version from ${meteorPackage.releaseName} package.js`)
@@ -34,7 +36,7 @@ async function applyVersion() {
     packageJsContent = packageJsContent.replace(PACKAGE_VERSION_REGEX, `version: '${release.newVersion}',`);
     await FS.writeFile(meteorPackage.packageJsPath, packageJsContent);
 
-    console.log(`✅  Changed version in package.js from v${currentVersion} to v${release.newVersion}\n`);
+    console.log(`✅  Changed ${meteorPackage.releaseName} (${packageName}) version from v${currentVersion} to v${release.newVersion}\n`);
 
     shell(`git add ${meteorPackage.packageJsPath}`);
     shell(`git commit -m 'Bump ${meteorPackage.releaseName} version to ${release.newVersion}'`);
