11607c0d8f0463b5788aa58c84ec026238032474
Jørgen Vatle <jorgen@wcaservices.net>
2023-11-24 01:53:31 +0200

    Update version requirement for meteor-vite npm package.

--- packages/vite-bundler/workers.ts
+++ packages/vite-bundler/workers.ts
@@ -122,7 +122,7 @@ class MeteorViteError extends Error {
     }
 }
 
-export const MIN_METEOR_VITE_NPM_VERSION = { major: 1, minor: 4, patch: 1 };
+export const MIN_METEOR_VITE_NPM_VERSION = { major: 1, minor: 5, patch: 0 };
 export const cwd = process.env.METEOR_VITE_CWD ?? guessCwd();
 export const meteorPackagePath = guessMeteorPackagePath();
 export const workerPath = Path.join(cwd, 'node_modules/meteor-vite/dist/bin/worker/index.mjs');
