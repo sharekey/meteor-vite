f492c01d5d4faf4013776cee0e90f0627558cd7c
Jørgen Vatle <jorgen@wcaservices.net>
2023-11-25 18:07:07 +0200

    Add MakeOptional utility type

--- npm-packages/meteor-vite/src/utilities/GenericTypes.ts
+++ npm-packages/meteor-vite/src/utilities/GenericTypes.ts
@@ -1,3 +1,8 @@
 export type DeepPartial<TObject> = TObject extends {} ? {
     [key in keyof TObject]?: DeepPartial<TObject[key]>
 } : TObject;
+
+export type MakeOptional<
+    TObject extends object,
+    TKeys extends keyof TObject
+> = Omit<TObject, TKeys> & Partial<Pick<TObject, TKeys>>;
