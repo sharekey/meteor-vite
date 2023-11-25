0740522db37612e427ed06a4138fc0ccfe57bf76
Jørgen Vatle <jorgen@wcaservices.net>
2023-11-25 21:33:50 +0200

    Configure Vite example using meteor-vite plugin rather than non-typed meteor object.

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { MeteorViteConfig } from '../../npm-packages/meteor-vite';
+import { meteor } from 'meteor-vite/plugin';
 
 export default defineConfig({
   plugins: [
     vue(),
+    meteor({
+      clientEntry: 'imports/ui/main.ts',
+      stubValidation: {
+        warnOnly: true,
+      }
+    }),
   ],
   optimizeDeps: {
     exclude: [
         'ts-minitest',
     ]
   },
-
-  meteor: {
-    clientEntry: 'imports/ui/main.ts',
-    stubValidation: {
-      warnOnly: true,
-    }
-  } satisfies MeteorViteConfig['meteor'],
 })
