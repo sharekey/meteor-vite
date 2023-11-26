58d8daa5d2dd7462ba8efa0563638b6bbd33852c
Jørgen Vatle <jorgen@wcaservices.net>
2023-11-26 03:16:52 +0200

    Allow environment variable overrides for background worker PID filepath.

--- npm-packages/meteor-vite/src/meteor/IPC/methods/vite-server.ts
+++ npm-packages/meteor-vite/src/meteor/IPC/methods/vite-server.ts
@@ -186,7 +186,7 @@ type WorkerRuntimeConfig = {
 
 class BackgroundWorker {
     public static instance: BackgroundWorker;
-    protected static readonly configPath = Path.join('.meteor', 'local', 'vite', 'vite-dev-server.pid')
+    protected static readonly configPath = process.env.BACKGROUND_WORKER_PID_PATH || Path.join('.meteor', 'local', 'vite', 'vite-dev-server.pid')
     public static async init(meteorParentPid: number) {
         if (BackgroundWorker.instance) {
             return BackgroundWorker.instance;
