9a80c9d0a4abca59dc54acc67556fe1521e06c95
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-16 01:41:55 +0100

    Add main Vue app component

---
+++ examples/vue-meteor-v3/imports/ui/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>Hello from Vue!</h2>
+    <div>
+      <h3>Vue links</h3>
+      <ul>
+        <li v-for="i in 5">...</li>
+      </ul>
+    </div>
+  </div>
+</template>
