e5701cb44c8e38eb407d83d34c52cb72866b118f
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-16 13:49:32 +0100

    Create step summary if possible in logger utility

--- packages/vite-bundler/utility/Logger.ts
+++ packages/vite-bundler/utility/Logger.ts
@@ -1,4 +1,6 @@
 import pc from 'picocolors';
+import { inspect } from 'node:util';
+import fs from 'node:fs';
 
 class Logger {
     protected debugEnabled = false;
@@ -13,19 +15,34 @@ class Logger {
             return Logger.DEBUG_ENV_TRIGGERS.includes(field.trim())
         });
     }
+    
+    protected addStepSummary(message: string, ...args: LogMethodArgs) {
+        const summaryFilePath = process.env.GITHUB_STEP_SUMMARY;
+        if (!summaryFilePath) {
+            return;
+        }
+        
+        const formattedArgs = args.length ? inspect(args) : '';
+        fs.appendFileSync(summaryFilePath, `⚡  ${message} ${formattedArgs}`);
+    }
+    
     public info(message: string, ...args: LogMethodArgs) {
+        this.addStepSummary(message, ...args);
         console.info(pc.blue(`⚡  ${message}`), ...args)
     }
     public error(message: string, ...args: LogMethodArgs) {
+        this.addStepSummary(message, ...args);
         console.error(pc.red(`⚡  ${message}`), ...args)
     }
     public success(message: string, ...args: LogMethodArgs) {
+        this.addStepSummary(message, ...args);
         console.log(pc.green(`⚡  ${message}`), ...args)
     }
     public debug(message: string, ...args: LogMethodArgs) {
         if (!this.debugEnabled) {
             return;
         }
+        this.addStepSummary(message, ...args);
         console.debug(pc.dim(pc.blue(`⚡  ${message}`)), ...args)
     }
 }
