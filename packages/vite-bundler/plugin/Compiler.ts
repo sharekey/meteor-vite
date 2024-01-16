3859fedbc29948a9dc8741e9f73bb8a4d8fa0389
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-16 08:05:31 +0100

    Add TypeScript implementation of the compiler plugin.

---
+++ packages/vite-bundler/plugin/Compiler.ts
@@ -0,0 +1,54 @@
+import Logger from '../utility/Logger';
+import Path from 'node:path';
+
+export default class Compiler {
+    public static cleanupHandlers: CleanupHandler[] = [];
+    processFilesForTarget(files: BuildPluginFile[]) {
+        files.forEach(file => {
+            Logger.debug(`Processing: ${file.getBasename()}`)
+            switch (Path.extname(file.getBasename())) {
+                case '.js':
+                    file.addJavaScript({
+                        path: file.getPathInPackage(),
+                        data: file.getContentsAsString(),
+                    })
+                    break
+                case '.css':
+                    file.addStylesheet({
+                        path: file.getPathInPackage(),
+                        data: file.getContentsAsString(),
+                    })
+                    break
+                default:
+                    file.addAsset({
+                        path: file.getPathInPackage(),
+                        data: file.getContentsAsBuffer(),
+                    })
+            }
+        })
+    }
+    
+    afterLink () {
+        Compiler.cleanupHandlers.forEach((handle) => handle());
+        Compiler.cleanupHandlers = [];
+    }
+    
+    static addCleanupHandler(handler: CleanupHandler) {
+        this.cleanupHandlers.push(handler);
+    }
+}
+type CleanupHandler = () => void;
+type PluginFileBuffer = ArrayBufferLike;
+interface BuildPluginFile {
+    getContentsAsString(): string;
+    getPathInPackage(): string;
+    getContentsAsBuffer(): PluginFileBuffer;
+    getBasename(): string;
+    addAsset(data: FileData): void;
+    addStylesheet(data: FileData): void;
+    addJavaScript(data: FileData): void;
+}
+interface FileData {
+    path: string;
+    data: string | PluginFileBuffer;
+}
