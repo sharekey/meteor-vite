6d55cccd9d5ab2abb5f02391394b32c2803903c4
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-16 07:51:16 +0100

    Add debug log method to logger.

--- packages/vite-bundler/utility/Logger.ts
+++ packages/vite-bundler/utility/Logger.ts
@@ -1,7 +1,12 @@
 type LogMethodArgs = unknown[];
 import pc from 'picocolors';
+import * as process from 'process';
 
 class Logger {
+    protected debugEnabled = false;
+    constructor() {
+        this.debugEnabled = ['true', '*', 'meteor-vite:*'].includes(process.env.DEBUG || 'false')
+    }
     public info(message: string, ...args: LogMethodArgs) {
         console.info(pc.blue(`⚡  ${message}`), ...args)
     }
@@ -10,6 +15,12 @@ class Logger {
     }
     public success(message: string, ...args: LogMethodArgs) {
         console.log(pc.green(`⚡  ${message}`), ...args)
+    }
+    public debug(message: string, ...args: LogMethodArgs) {
+        if (!this.debugEnabled) {
+            return;
+        }
+        console.log(pc.dim(pc.blue(`⚡  ${message}`)), ...args)
     }
 }
 
