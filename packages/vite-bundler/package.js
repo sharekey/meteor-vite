2b7a2b9089f4709449498082b36d564d7be0cc81
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-16 01:46:25 +0100

    Allow beta version of caching-compiler

--- packages/vite-bundler/package.js
+++ packages/vite-bundler/package.js
@@ -10,7 +10,7 @@ Package.registerBuildPlugin({
   name: 'vite',
   use: [
     'ecmascript@0.16.2 || 1.0.0',
-    'caching-compiler@1.2.2',
+    'caching-compiler@1.2.2 || 2.0.0-beta300.0',
     'babel-compiler@7.9.0',
     'typescript@3.0.0 || 4.0.0 || 5.0.0',
   ],
