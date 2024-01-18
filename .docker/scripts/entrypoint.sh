560d51799372f7db810fc03cdc7f9bfa2c607ba0
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-18 05:11:41 +0100

    Run meteor-base's default mongo connection script in entrypoint.sh

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 set -o errexit
+cd $SCRIPS_FOLDER
+
+MONGO_URL=$(echo "$MONGO_URL" | MONGO_DATABASE="$APP_BASENAME" envsubst)
+
+# Poll until we can successfully connect to MongoDB
+source ./connect-to-mongo.sh
 
 echo 'Starting app...'
 cd $APP_BUNDLE_FOLDER/bundle
-
-MONGO_URL=$(echo "$MONGO_URL" | MONGO_DATABASE="$APP_BASENAME" envsubst)
 export MONGO_URL
 
 exec "$@"
