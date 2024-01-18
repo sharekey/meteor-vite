71a0c1f75d451c4445f0dd81238b9235f781ccfb
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-18 06:36:43 +0100

    Sort links by creation date.

--- examples/meteor-v3-vue/imports/api/links/server/links.publications.ts
+++ examples/meteor-v3-vue/imports/api/links/server/links.publications.ts
@@ -3,6 +3,9 @@ import LinksCollection from '../links.collection';
 
 Meteor.publish('links', () => {
     return LinksCollection.find({}, {
+        sort: {
+            createdAt: -1,
+        },
         limit: 50,
     })
 })
