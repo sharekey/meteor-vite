512182eadbc528281ffa3022fd230b5f0385d65d
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-18 06:36:00 +0100

    Add link schema to collection module

--- examples/meteor-v3-vue/imports/api/links/links.collection.ts
+++ examples/meteor-v3-vue/imports/api/links/links.collection.ts
@@ -1,3 +1,9 @@
 import { Mongo } from 'meteor/mongo';
+import Valibot from 'valibot';
 
 export default new Mongo.Collection('links');
+
+export const LinkSchema = Valibot.object({
+    url: Valibot.string([Valibot.maxLength(512)]),
+    title: Valibot.string([Valibot.maxLength(512)]),
+});
