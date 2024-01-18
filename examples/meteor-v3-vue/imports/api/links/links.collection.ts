d9fab39c773d65130cac415e0b15ed9b1e42abb1
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-18 06:38:58 +0100

    Add document type for links.

--- examples/meteor-v3-vue/imports/api/links/links.collection.ts
+++ examples/meteor-v3-vue/imports/api/links/links.collection.ts
@@ -1,9 +1,15 @@
 import { Mongo } from 'meteor/mongo';
 import Valibot from 'valibot';
 
-export default new Mongo.Collection('links');
+export default new Mongo.Collection<LinkDocument>('links');
 
 export const LinkSchema = Valibot.object({
     url: Valibot.string([Valibot.maxLength(512)]),
     title: Valibot.string([Valibot.maxLength(512)]),
 });
+
+export interface LinkDocument {
+    _id: string;
+    url: string;
+    title: string;
+}
