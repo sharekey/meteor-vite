0d75901a28ff950f08fd2a68216e753055043362
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-18 05:21:26 +0100

    Add export for interpolated Mongo connection URI

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -4,12 +4,12 @@ set -o errexit
 cd $SCRIPTS_FOLDER
 
 MONGO_URL=$(echo "$MONGO_URL" | MONGO_DATABASE="$APP_BASENAME" envsubst)
+export MONGO_URL
 
 # Poll until we can successfully connect to MongoDB
 source ./connect-to-mongo.sh
 
 echo 'Starting app...'
 cd $APP_BUNDLE_FOLDER/bundle
-export MONGO_URL
 
 exec "$@"
