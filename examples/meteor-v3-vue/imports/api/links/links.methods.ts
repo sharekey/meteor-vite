d2dea20f297d7d7868f61af68222c1548ac6c004
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-18 06:36:26 +0100

    Validate input for link methods

--- examples/meteor-v3-vue/imports/api/links/links.methods.ts
+++ examples/meteor-v3-vue/imports/api/links/links.methods.ts
@@ -1,8 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import LinksCollection from './links.collection';
+import LinksCollection, { LinkSchema } from './links.collection';
+import * as Valibot from 'valibot';
 
 Meteor.methods({
-    'links.create'(link: { title: string, url: string }) {
-        LinksCollection.insertAsync(link);
+    'links.create'(request: { title: string, url: string }) {
+        const link = Valibot.parse(LinkSchema, request);
+        return LinksCollection.insertAsync({
+            ...link,
+            createdAt: new Date()
+        });
     }
 })
