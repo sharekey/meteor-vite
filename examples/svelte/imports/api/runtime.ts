244e4bab0bfb8b5cceed5f3d20c9cd4f09a586b5
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-18 07:19:01 +0100

    Export runtime collection as RuntimeCollection

--- examples/svelte/imports/api/runtime.ts
+++ examples/svelte/imports/api/runtime.ts
@@ -1,16 +1,16 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
 
-const Runtime = new Mongo.Collection<RuntimeDocument>('runtime');
+const RuntimeCollection = new Mongo.Collection<RuntimeDocument>('runtime');
 
 Meteor.publish('runtime', () => {
-    return Runtime.find();
+    return RuntimeCollection.find();
 });
 
 Meteor.methods({
     'runtime.click': () => {
-        const clicks = Runtime.findOne({ _id: 'clicks' })?.value || 0;
-        Runtime.upsert({ _id: 'clicks' }, { $set: { value: clicks + 1 } });
+        const clicks = RuntimeCollection.findOne({ _id: 'clicks' })?.value || 0;
+        RuntimeCollection.upsert({ _id: 'clicks' }, { $set: { value: clicks + 1 } });
     }
 })
 
@@ -19,4 +19,4 @@ export interface RuntimeDocument {
     value: number;
 }
 
-export default Runtime;
+export default RuntimeCollection;
