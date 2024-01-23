43002d88ae282e8724bf3246468bd4d717c8fc5f
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-23 16:52:07 +0100

    Correct import path for API modules

--- examples/.example-template/imports/entrypoints/meteor-server.ts
+++ examples/.example-template/imports/entrypoints/meteor-server.ts
@@ -1,6 +1,6 @@
 // Register publications and methods
-import '../imports/api/links/links.methods';
-import '../imports/api/links/links.publications';
+import '../api/links/links.methods';
+import '../api/links/links.publications';
 
 // Import server main module
 import '../../server/main';
