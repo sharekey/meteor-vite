a33ab78746b5c99984acf2256ea4bbe521cb709f
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-28 13:18:06 +0100

    Add type declaration for plugin registration

--- packages/vite-bundler/env.d.ts
+++ packages/vite-bundler/env.d.ts
@@ -2,6 +2,13 @@ declare global {
     interface PromiseConstructor {
         await<T>(promise: Promise<T>): T;
     }
+    
+    namespace Plugin {
+        function registerCompiler(config: {
+            extensions: string[];
+            filenames: string[];
+        }, compilerFactory: () => object): void;
+    }
 }
 
 declare module 'meteor/meteor' {
