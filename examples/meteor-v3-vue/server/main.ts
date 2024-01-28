f41a915e59e9a001c7b4a604b9b2173d2f690f98
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-28 20:14:02 +0100

    Import links publications and methods

--- examples/meteor-v3-vue/server/main.ts
+++ examples/meteor-v3-vue/server/main.ts
@@ -1,6 +1,8 @@
 import { Meteor } from "meteor/meteor";
 import { onPageLoad } from "meteor/server-render";
 import LinksCollection from '../imports/api/links/links.collection';
+import '../imports/api/links/links.methods';
+import '../imports/api/links/server/links.publications';
 
 Meteor.startup(async () => {
   // Code to run on server startup.
