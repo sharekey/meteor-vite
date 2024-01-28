5c21e0e4c51279114b056a1791368296dce58a19
Jørgen Vatle <Jorgen@WcAServices.net>
2024-01-28 10:46:23 +0100

    Add missing type to message argument

--- packages/vite-bundler/utility/Errors.ts
+++ packages/vite-bundler/utility/Errors.ts
@@ -1,5 +1,5 @@
 export class MeteorViteError extends Error {
-    constructor(message, details?: { cause?: unknown }) {
+constructor(message: string, details?: { cause?: unknown }) {
         super(
             `⚡  ${message}`,
             // @ts-expect-error Might or might not be supported depending on Meteor's node version.
