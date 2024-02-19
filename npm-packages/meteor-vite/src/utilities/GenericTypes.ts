44b5253aa3078dbe47a8fa7466923925bcd822c2
Jørgen Vatle <Jorgen@WcAServices.net>
2024-02-19 14:39:57 +0100

    Add MakeRequired utility type

--- npm-packages/meteor-vite/src/utilities/GenericTypes.ts
+++ npm-packages/meteor-vite/src/utilities/GenericTypes.ts
@@ -6,3 +6,5 @@ export type MakeOptional<
     TObject extends object,
     TKeys extends keyof TObject
 > = Omit<TObject, TKeys> & Partial<Pick<TObject, TKeys>>;
+
+export type MakeRequired<TObject extends object, TKeys extends keyof TObject> = Omit<TObject, TKeys> & Required<Pick<TObject, TKeys>>;
