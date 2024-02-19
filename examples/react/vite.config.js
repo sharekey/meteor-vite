bde172a2d29988ff9a9d86610061bc7e9c798621
Jørgen Vatle <Jorgen@WcAServices.net>
2024-02-19 21:40:24 +0100

    Update Meteor-Vite plugin import so it's usable outside of this repository by default

--- examples/react/vite.config.js
+++ examples/react/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { meteor } from '../../npm-packages/meteor-vite/src/plugin';
+import { meteor } from 'meteor-vite/plugin';
 
 export default defineConfig({
   plugins: [
