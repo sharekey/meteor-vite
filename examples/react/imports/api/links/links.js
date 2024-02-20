63d3c60ee5adcdeaf1b63596c8d919dab4ad88e6
Jørgen Vatle <Jorgen@WcAServices.net>
2024-02-20 20:19:37 +0100

    Move insertLink method to links.js

--- examples/react/imports/api/links/links.js
+++ examples/react/imports/api/links/links.js
@@ -5,6 +5,10 @@ export const LinksCollection = new Mongo.Collection('links');
 
 Meteor.methods({
     async 'links.create'(link) {
-        await LinksCollection.insertAsync(link);
+        await insertLink(link)
     }
 })
+
+export async function insertLink({ title, url }) {
+    await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
+}
