382afed15ee04d926de881b3efc20c70960d23c6
Jørgen Vatle <Jorgen@WcAServices.net>
2024-02-20 20:22:40 +0100

    Add link insertion example to info component

--- examples/react/imports/ui/components/Info.jsx
+++ examples/react/imports/ui/components/Info.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { useFind, useSubscribe } from '@meteor-vite/react-meteor-data';
 import { LinksCollection } from '../../api/links/links';
+import { Meteor } from 'meteor/meteor';
 
 export const Info = () => {
     try {
         const isLoading = useSubscribe('links');
         const links = useFind(() => LinksCollection.find());
+        const form = new class {
+            data = {
+                title: '',
+                url: '',
+            }
+            async submit(event) {
+                event.preventDefault();
+                await Meteor.callAsync('links.create', this.data);
+            }
+            setData(field) {
+                if (!field in this.data) {
+                    throw new Error('Unknown field provided!');
+                }
+                return (event) => this.data[field] = event.target.value;
+            }
+        }
         if (isLoading()) {
             return <div>Loading...</div>;
         }
@@ -18,6 +35,18 @@ export const Info = () => {
                         <a href={link.url} target="_blank">{link.title}</a>
                     </li>,
                 )}</ul>
+                <h3>Add resources</h3>
+                <form onSubmit={e => form.submit(e)}>
+                    <label>
+                        Title
+                        <input name="title" onChange={form.setData('title')} placeholder="My awesome resource.." />
+                    </label>
+                    <label>
+                        URL
+                        <input name="url" onChange={form.setData('url')} placeholder="http://examp.." />
+                    </label>
+                    <button type="submit">Add</button>
+                </form>
             </div>
         );
 
