b3c2a3452d2cae59be19ab9ce2e67f506f9272bb
Jørgen Vatle <Jorgen@WcAServices.net>
2024-02-20 20:02:35 +0100

    Add link creation method`

--- examples/react/imports/api/links/links.js
+++ examples/react/imports/api/links/links.js
@@ -1,3 +1,10 @@
 import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
 
 export const LinksCollection = new Mongo.Collection('links');
+
+Meteor.methods({
+    async 'links.create'(link) {
+        await LinksCollection.insertAsync(link);
+    }
+})
