055d03d087706b954b60de372edb51747e07a090
Jørgen Vatle <Jorgen@WcAServices.net>
2024-03-12 04:18:50 +0100

    Transform publications/methods using Zodern's Babel plugin

--- npm-packages/zodern-relay/src/Plugin.ts
+++ npm-packages/zodern-relay/src/Plugin.ts
@@ -1,9 +1,8 @@
+import Babel from '@babel/core';
 import FS from 'fs';
 import Path from 'path';
 import type { Plugin } from 'vite';
 
-const sourceModule = 'meteor/zodern:relay';
-const stubModule = '@meteor-vite/zodern-relay/stubs/relay-client';
 const cwd = process.cwd();
 const defaultOptions: Options = {
     publicationDirectories: [
@@ -22,45 +21,6 @@ export default async function zodernRelay({
         methods: methodDirectories.map((path) => Path.relative(cwd, path)),
         publications: publicationDirectories.map((path) => Path.relative(cwd, path)),
     }
-    const packageJson: {
-        meteor: {
-            mainModule: {
-                client: string;
-            }
-        }
-    } = JSON.parse(FS.readFileSync(Path.join(cwd, 'package.json'), 'utf-8'));
-    const relayImportsFilename = '__relay-imports.js';
-    const clientMainModule = Path.resolve(packageJson.meteor.mainModule.client);
-    const clientRootDir = Path.dirname(clientMainModule);
-    const relayImportsModule = Path.join(clientRootDir, relayImportsFilename);
-    
-    
-    // Prepare relay imports module
-    // This prevents Meteor from omitting exported publications/methods from zodern:relay
-    {
-        if (!FS.existsSync(relayImportsModule)) {
-            FS.writeFileSync(relayImportsModule, `// zodern:relay imports - added by Vite (You should commit this file)`);
-        }
-        const mainModuleContent = FS.readFileSync(clientMainModule, 'utf-8');
-        const importPath = Path.relative(clientRootDir, relayImportsFilename);
-        if (!mainModuleContent.includes(relayImportsFilename)) {
-            FS.writeFileSync(clientMainModule, `import ${JSON.stringify(importPath)}\n${mainModuleContent}`);
-        }
-    }
-    
-    function addRelayImport(relay: RelayInfo) {
-        const importPath = Path.relative(clientMainModule, relay.relativePath);
-        const existingContent = FS.readFileSync(relayImportsModule, 'utf-8');
-        const lines = existingContent.split(/[\r\n]+/);
-        for (const line of lines) {
-            if (line.includes(importPath)) {
-                return;
-            }
-        }
-        lines.push(`import ${JSON.stringify(importPath)}`);
-        FS.writeFileSync(relayImportsModule, lines.join('\n'));
-    }
-    
     type RelayInfo = {
         type: 'methods' | 'publications';
         id: string;
@@ -101,22 +61,26 @@ export default async function zodernRelay({
             return `\0${id}`;
         },
         
-        load(id) {
-            const [_, module] = id.split('\0');
-            const relay = resolveRelay(module || '');
+        async load(filename) {
+            const relay = resolveRelay(filename || '');
             if (!relay) {
                 return;
             }
+            const code = FS.readFileSync(filename, 'utf-8');
+            const transform = await Babel.transformAsync(code, {
+                configFile: false,
+                babelrc: false,
+                filename,
+                plugins: ['@zodern/babel-plugin-meteor-relay']
+            });
             
-            addRelayImport(relay);
+            if (!transform) {
+                return;
+            }
             
-            // Load target module content
-            // Traverse module
-            // Get exports
-            // Filter out non-zodern:relay exports
-            // Track all method/publication name keys
-            // Map createPublication/createMethod calls to _createClient<Publication/Method>(<publicationName/methodName>>)
-            // Return transformed content
+            return {
+                code: transform.code ?? '',
+            }
         }
     }
 }
