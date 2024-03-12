755b109b6a89bd87ac6e013c30ca93b4f45b1260
Jørgen Vatle <Jorgen@WcAServices.net>
2024-03-12 01:30:48 +0100

    Add publication/method directory options

--- npm-packages/zodern-relay/src/Plugin.ts
+++ npm-packages/zodern-relay/src/Plugin.ts
@@ -2,8 +2,20 @@ import type { Plugin } from 'vite';
 
 const sourceModule = 'meteor/zodern:relay';
 const stubModule = '@meteor-vite/zodern-relay/stubs/relay-client';
+const defaultOptions: Options = {
+    publicationDirectories: [
+        './imports/publications',
+    ],
+    methodDirectories: [
+        './imports/methods',
+    ],
+}
 
-export default async function zodernRelay(): Promise<Plugin> {
+export default async function zodernRelay({
+    publicationDirectories = [...defaultOptions.publicationDirectories],
+    methodDirectories = [...defaultOptions.methodDirectories],
+}: Options = defaultOptions): Promise<Plugin> {
+    
     return {
         name: 'zodern-relay',
         resolveId(id) {
@@ -21,4 +33,20 @@ export default async function zodernRelay(): Promise<Plugin> {
             return `export * from ${JSON.stringify(stubModule)}`;
         }
     }
+}
+
+export interface Options {
+    /**
+     * Path to directories where your zodern:relay methods live
+     * @default
+     * publicationDirectories: ['./imports/methods']
+     */
+    methodDirectories: string[];
+    
+    /**
+     * Path to the directories where your zodern:relay publications live.
+     * @default
+     * publicationDirectories: ['./imports/publications']
+     */
+    publicationDirectories: string[];
 }
