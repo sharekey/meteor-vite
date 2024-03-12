ea1ccbe6617306a2eb4b8c1ab6bcf3830560058a
Jørgen Vatle <Jorgen@WcAServices.net>
2024-03-12 01:58:49 +0100

    Add pseudocode for zodern:relay Vite implementation

--- npm-packages/zodern-relay/src/Plugin.ts
+++ npm-packages/zodern-relay/src/Plugin.ts
@@ -1,7 +1,9 @@
+import Path from 'path';
 import type { Plugin } from 'vite';
 
 const sourceModule = 'meteor/zodern:relay';
 const stubModule = '@meteor-vite/zodern-relay/stubs/relay-client';
+const cwd = process.cwd();
 const defaultOptions: Options = {
     publicationDirectories: [
         './imports/publications',
@@ -15,22 +17,58 @@ export default async function zodernRelay({
     publicationDirectories = [...defaultOptions.publicationDirectories],
     methodDirectories = [...defaultOptions.methodDirectories],
 }: Options = defaultOptions): Promise<Plugin> {
+    const directories = {
+        methods: methodDirectories.map((path) => Path.relative(cwd, path)),
+        publications: publicationDirectories.map((path) => Path.relative(cwd, path)),
+    }
+    
+    function resolveRelay(id: string) {
+        const relativePath = Path.relative(cwd, id);
+        for (const dir of directories.methods) {
+            if (!relativePath.startsWith(dir)) {
+                continue;
+            }
+            return {
+                id,
+                type: 'methods',
+                relativePath,
+            }
+        }
+        for (const dir of directories.publications) {
+            if (!relativePath.startsWith(dir)) {
+                continue;
+            }
+            return {
+                id,
+                type: 'publications',
+                relativePath,
+            }
+        }
+    }
+    
     
     return {
         name: 'zodern-relay',
         resolveId(id) {
-            if (!id.startsWith(sourceModule)) {
+            if (!resolveRelay(id)) {
                 return;
             }
             return `\0${id}`;
         },
         load(id) {
-            if (!id.startsWith(`\0${sourceModule}`)) {
+            const [_, module] = id.split('\0');
+            const relay = resolveRelay(module || '');
+            if (!relay) {
                 return;
             }
             
-            // language=typescript
-            return `export * from ${JSON.stringify(stubModule)}`;
+            // Load target module content
+            // Traverse module
+            // Get exports
+            // Filter out non-zodern:relay exports
+            // Track all method/publication name keys
+            // Map createPublication/createMethod calls to _createClient<Publication/Method>(<publicationName/methodName>>)
+            // Return transformed content
         }
     }
 }
