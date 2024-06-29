5eedd81c301970fe04e1b32310d5671a064db6b2
Jørgen Vatle <jorgen@wcaservices.net>
2024-06-30 00:46:52 +0300

    Correct misleading minimum version requirement notice.
    https://github.com/meteor/meteor/issues/12937

--- packages/vite-bundler/workers.ts
+++ packages/vite-bundler/workers.ts
@@ -119,7 +119,7 @@ class MeteorViteError extends Error {
     }
 }
 
-export const MIN_METEOR_VITE_NPM_VERSION = { major: 1, minor: 8, patch: 0 };
+export const MIN_METEOR_VITE_NPM_VERSION = { major: 1, minor: 10, patch: 3 };
 export const cwd = process.env.METEOR_VITE_CWD ?? guessCwd();
 export const workerPath = Path.join(cwd, 'node_modules/meteor-vite/dist/bin/worker.mjs');
 export function getProjectPackageJson(): ProjectJson {
