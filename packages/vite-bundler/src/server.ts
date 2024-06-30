7e84bd606fe9a9707b217ea0c8bb22457e9e0e08
Jørgen Vatle <jorgen@wcaservices.net>
2024-07-01 01:31:56 +0300

    Prevent Meteor from serving 200 OK for routes that should actually 404

--- packages/vite-bundler/src/server.ts
+++ packages/vite-bundler/src/server.ts
@@ -1,5 +1,7 @@
 import HTTP from 'http';
-import { WebAppInternals } from 'meteor/webapp';
+import { WebApp, WebAppInternals } from 'meteor/webapp';
+import { VITE_BASE } from './utility/Helpers';
+import Logger from './utility/Logger';
 import type { BoilerplateData } from './vite-boilerplate/common';
 import { ViteDevServerWorker } from './vite-boilerplate/development';
 import { ViteProductionBoilerplate } from './vite-boilerplate/production';
@@ -24,3 +26,16 @@ WebAppInternals.registerBoilerplateDataCallback('meteor-vite', async (request: H
         data.dynamicBody = `${data.dynamicBody || ''}\n${dynamicBody}`;
     }
 });
+
+Meteor.startup(() => {
+    if (worker instanceof ViteProductionBoilerplate) {
+        Logger.debug(`Vite asset base URL: /${VITE_BASE}`);
+        
+        // Prevent Meteor from sending a 200 OK HTML file when the request is clearly not valid.
+        // If an asset is found by Meteor, this hook will not be called.
+        WebApp.connectHandlers.use(`/${VITE_BASE}`, (req, res, next) => {
+            res.writeHead(404, 'Not found');
+            res.end();
+        })
+    }
+})
