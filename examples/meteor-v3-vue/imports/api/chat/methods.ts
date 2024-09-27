ffe5f49a439f95bdd9b4867f7dbf7a2e939c3bb0
Jørgen Vatle <jorgen@wcaservices.net>
2024-09-27 22:12:15 +0300

    Add message send method

---
+++ examples/meteor-v3-vue/imports/api/chat/methods.ts
@@ -0,0 +1,16 @@
+import { createMethod } from 'meteor/zodern:relay';
+import { z } from 'zod';
+import { ChatCollection } from './collection';
+
+export const sendMessage = createMethod({
+    name: 'sendMessage',
+    schema: z.object({
+        content: z.string(),
+    }),
+    run({ content }) {
+        return ChatCollection.insertAsync({
+            content,
+            createdAt: new Date(),
+        });
+    }
+});
