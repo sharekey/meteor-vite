9cef1e9dc9b6d6b6c34d4b496df4a03a9df9071f
Jørgen Vatle <jorgen@wcaservices.net>
2024-09-27 23:20:32 +0300

    Add content patterns

--- examples/meteor-v3-vue/tailwind.config.js
+++ examples/meteor-v3-vue/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      "client/**/*.html",
+      "imports/**/*.{vue,ts,js}"
+  ],
   theme: {
     extend: {},
   },
