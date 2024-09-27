192ec66bbca25b56da685992971e403e56d7a532
Jørgen Vatle <jorgen@wcaservices.net>
2024-09-28 00:36:02 +0300

    Add auto imports for zodern:relay

--- examples/meteor-v3-vue/client/main.js
+++ examples/meteor-v3-vue/client/main.js
@@ -1,1 +1,12 @@
+/**
+ * These modules are automatically imported by jorgenvatle:vite-bundler.
+ * You can commit these to your project or move them elsewhere if you'd like,
+ * but they must be imported somewhere in your Meteor entrypoint file.
+ *
+ * More info: https://github.com/JorgenVatle/meteor-vite#lazy-loaded-meteor-packages
+**/
+import 'meteor/zodern:relay';
+import 'meteor/zodern:relay/client';
+/** End of vite-bundler auto-imports **/
+
 console.log(`Greetings from ${module.id}!`);
