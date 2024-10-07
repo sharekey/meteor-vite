8b632514c13199a3ef9b95e360b7836bf4dabdfb
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-07 16:14:10 +0300

    Allow worker messages through environment variables rather than just IPC

--- npm-packages/meteor-vite/src/bin/worker.ts
+++ npm-packages/meteor-vite/src/bin/worker.ts
@@ -2,7 +2,7 @@ import { createErrorHandler } from '../error/ErrorHandler';
 import { validateIpcChannel } from '../meteor/IPC/interface';
 import IpcMethods, { WorkerMethod } from '../meteor/IPC/methods';
 
-process.on('message', async (message: WorkerMethod) => {
+async function handleMessage(message: WorkerMethod) {
     if (!message || !message.method) {
         console.error('Vite: Unrecognized worker IPC message', { message });
         return;
@@ -20,7 +20,14 @@ process.on('message', async (message: WorkerMethod) => {
     }, ...message.params as [params: any]).catch(
         createErrorHandler('Vite: worker process encountered an exception!')
     );
-})
+}
 
+process.on('message', async (message: WorkerMethod) => handleMessage(message));
 
-validateIpcChannel(process.send);
+if (process.env.WORKER_METHOD) {
+    const message: WorkerMethod = JSON.parse(process.env.WORKER_METHOD);
+    handleMessage(message);
+} else {
+    validateIpcChannel(process.send);
+}
+
