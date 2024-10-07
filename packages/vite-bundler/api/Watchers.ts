5e9a87fc11f38e22e7e6f66b704d832bab399dc3
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-07 12:42:49 +0300

    Subscribe to log collection on the client

--- packages/vite-bundler/api/Watchers.ts
+++ packages/vite-bundler/api/Watchers.ts
@@ -1,6 +1,15 @@
+import { Meteor } from 'meteor/meteor';
 import { DataStreamCollection } from './Collections';
 
 export function watchDataStreamLogs() {
+    if (Meteor.isProduction) {
+        throw new Error('meteor-vite data logs are only available in development mode');
+    }
+    
+    if (Meteor.isClient) {
+        Meteor.subscribe('meteor-vite:log');
+    }
+    
     DataStreamCollection.find({}).observe({
         added(document) {
             console.log('New log:', document);
