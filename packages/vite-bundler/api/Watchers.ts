9e35116b48817fe77d9f846e44ba9ffd29de223c
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-07 12:47:31 +0300

    Add onReady/onStop callbacks for log subscription

--- packages/vite-bundler/api/Watchers.ts
+++ packages/vite-bundler/api/Watchers.ts
@@ -7,7 +7,17 @@ export function watchDataStreamLogs() {
     }
     
     if (Meteor.isClient) {
-        Meteor.subscribe('meteor-vite:log');
+        Meteor.subscribe('meteor-vite:log', {
+            onReady() {
+                console.debug('⚡ Listening for logs from meteor-vite');
+            },
+            onStop(error: unknown) {
+                if (!error) {
+                    return console.debug('⚡ Unsubscribed from meteor-vite logs');
+                }
+                console.debug('⚡ Error from meteor-vite logs publication', error);
+            }
+        });
     }
     
     DataStreamCollection.find({}).observe({
