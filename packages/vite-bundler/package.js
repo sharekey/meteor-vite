c184da8e09f7e41bad67b2d2e96f715ba3baddc5
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-17 13:51:52 +0300

    Bump version

--- packages/vite-bundler/package.js
+++ packages/vite-bundler/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jorgenvatle:vite-bundler',
-  version: '2.1.0',
+  version: '2.1.1',
   summary: 'Integrate the Vite.js bundler with Meteor',
   git: 'https://github.com/JorgenVatle/meteor-vite',
   documentation: 'README.md',
