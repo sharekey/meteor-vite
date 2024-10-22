b5e2a8ea236195f09b0ed67e35ad7d9a024d1de8
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-22 19:03:42 +0300

    Refactor links component to be a separate page

--- examples/vue-ssr/imports/ui/pages/Links.vue
+++ examples/vue-ssr/imports/ui/pages/Links.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { LinksCollection } from '/imports/api/links'
 import VueMeteor from '/imports/ui/lib/VueMeteor';
 
--- examples/vue-ssr/imports/ui/router.ts
+++ examples/vue-ssr/imports/ui/router.ts
@@ -1,3 +1,4 @@
+import Links from '/imports/ui/pages/Links.vue';
 import { type RouteRecordRaw } from 'vue-router';
 import About from './pages/About.vue';
 import Home from './pages/Home.vue';
@@ -13,4 +14,9 @@ export const routes = [
     name: 'about',
     component: About,
   },
+  {
+    path: '/links',
+    name: 'links',
+    component: Links,
+  }
 ] satisfies RouteRecordRaw[];
