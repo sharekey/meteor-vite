e4c9ed70b3aa7158bc95081f1c2927942e7cd5ce
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-22 18:05:34 +0300

    Add UI server module

---
+++ examples/vue-ssr/imports/ui/server.ts
@@ -0,0 +1,20 @@
+import { Meteor } from 'meteor/meteor';
+import { WebAppInternals } from 'meteor/webapp';
+import { createApp } from './main';
+import { renderToString } from 'vue/server-renderer';
+
+if (!Meteor.isServer) {
+    throw new Meteor.Error('server-only', 'UI server module can only be accessed by the Meteor server!');
+}
+
+WebAppInternals.registerBoilerplateDataCallback('vue-ssr', async (req, data) => {
+    const { app, router } = createApp();
+    
+    await router.push(req.url || '/');
+    const context = {};
+    
+    const html = await renderToString(app, context);
+    
+    const oldBody = data.dynamicBody || '';
+    data.dynamicBody = `${oldBody}\n${html}`;
+})
