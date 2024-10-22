3957ab0a986e5fc595cc4b2a8f8ef8517b0c5467
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-22 17:42:28 +0300

    Only export routes from router module

--- examples/vue-ssr/imports/ui/router.ts
+++ examples/vue-ssr/imports/ui/router.ts
@@ -1,19 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from './Home.vue'
-import About from './About.vue'
+import { type RouteRecordRaw } from 'vue-router';
+import About from './About.vue';
+import Home from './Home.vue';
 
-export const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
-  ],
-})
+export const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About,
+  },
+] satisfies RouteRecordRaw[];
