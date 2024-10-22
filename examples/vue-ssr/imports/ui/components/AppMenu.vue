21bf0180071d62d50106f34a80874c049f314ff5
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-22 19:14:07 +0300

    Render route links using router definitions

--- examples/vue-ssr/imports/ui/components/AppMenu.vue
+++ examples/vue-ssr/imports/ui/components/AppMenu.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import { routes } from '/imports/ui/router';
+</script>
+
 <template>
   <nav class="flex items-center">
-    <router-link :to="{ name: 'home' }" class="mr-4 text-green-700 hover:underline">Home</router-link>
-    <router-link :to="{ name: 'about' }" class="mr-4 text-green-700 hover:underline">About</router-link>
+    <router-link v-for="route in routes" :to="{ name: route.name }" class="mr-4 text-green-700 hover:underline capitalize">
+      {{ route.name }}
+    </router-link>
   </nav>
 </template>
