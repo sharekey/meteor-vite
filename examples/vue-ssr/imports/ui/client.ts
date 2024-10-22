cf1ff5210a241703788ae9e6e1bf32ea22bf73bd
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-22 18:06:34 +0300

    Validate context for client UI module

--- examples/vue-ssr/imports/ui/client.ts
+++ examples/vue-ssr/imports/ui/client.ts
@@ -2,6 +2,10 @@ import { createApp } from '/imports/ui/main';
 import { Meteor } from 'meteor/meteor';
 import { VueMeteor } from 'vue-meteor-tracker';
 
+if (!Meteor.isClient) {
+    throw new Meteor.Error('client-only', 'The UI client module should only be imported by clients!');
+}
+
 Meteor.startup(() => {
     const { app } = createApp();
     app.use(VueMeteor);
