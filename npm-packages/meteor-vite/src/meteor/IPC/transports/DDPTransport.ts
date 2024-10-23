cc9bc55532e4d42966a3783802721e9ae001d4ed
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-23 13:47:42 +0300

    Add DDPTransport

---
+++ npm-packages/meteor-vite/src/meteor/IPC/transports/DDPTransport.ts
@@ -0,0 +1,20 @@
+import type { DDPConnection } from '../DDP';
+import type { WorkerResponse } from '../methods';
+import type { IncomingMessageHandler, IpcTransport } from './Transport';
+
+export class DDPTransport implements IpcTransport {
+    constructor(protected ddp: DDPConnection) {
+    }
+    
+    public listen(handler: IncomingMessageHandler) {
+        this.ddp.onIpcCall(message => handler(message));
+    }
+    
+    public async reply(message: WorkerResponse) {
+        await this.ddp.ipcReply(message)
+    }
+    
+    public get active() {
+        return this.ddp.status.connected;
+    }
+}
