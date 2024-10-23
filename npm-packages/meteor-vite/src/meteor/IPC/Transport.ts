ddcafa32a54561d6c6b7a7bfe340d77cfeaafbbb
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-23 13:05:51 +0300

    Add statically defined IPC replies interface

--- npm-packages/meteor-vite/src/meteor/IPC/Transport.ts
+++ npm-packages/meteor-vite/src/meteor/IPC/Transport.ts
@@ -1,5 +1,7 @@
 import { createErrorHandler } from '../../error/ErrorHandler';
+import type { WorkerRuntimeConfig } from './BackgroundWorker';
 import IpcMethods, { WorkerMethod, type WorkerResponse } from './methods';
+import type { ViteRuntimeConfig } from './methods/vite-server';
 
 type IncomingMessageHandler = (message: WorkerMethod) => Promise<void>;
 
@@ -23,4 +25,21 @@ export async function defineIpcTransport(adapter: {
             createErrorHandler('Vite: worker process encountered an exception!')
         );
     })
+}
+
+
+export interface IpcReplies {
+    buildResult: {
+        payload:
+            | { success: false }
+            | {
+                  success: true;
+                  outDir: string;
+                  meteorViteConfig: any,
+                  output?: { name?: string, type: string, fileName: string }[]
+              };
+    }
+    viteConfig: ViteRuntimeConfig
+    refreshNeeded: void,
+    workerConfig: WorkerRuntimeConfig & { listening: boolean }
 }
