ec97068dd80b3c0d519b0884150951bd87f4f94a
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-23 14:27:00 +0300

    Start listeners from IPCAdapter

--- npm-packages/meteor-vite/src/meteor/IPC/transports/Transport.ts
+++ npm-packages/meteor-vite/src/meteor/IPC/transports/Transport.ts
@@ -13,26 +13,6 @@ export abstract class IpcTransport {
     }
 }
 
-export async function defineIpcTransport(adapter: IpcTransport) {
-    IPC.addTransport(adapter);
-    await adapter.listen(async (message) => {
-        if (!message || !message.method) {
-            console.error('Vite: Unrecognized worker IPC message', { message });
-            return;
-        }
-        
-        const callWorkerMethod = IpcMethods[message.method];
-        
-        if (typeof callWorkerMethod !== 'function') {
-            console.error(`Vite: The provided IPC method hasn't been defined yet!`, { message });
-        }
-        
-        await callWorkerMethod((response) => adapter.reply(response), ...message.params as [params: any]).catch(
-            createErrorHandler('Vite: worker process encountered an exception!')
-        );
-    })
-}
-
 class IPCAdapter {
     protected transports: Set<IpcTransport> = new Set();
     constructor() {}
@@ -53,6 +33,31 @@ class IPCAdapter {
     
     public hasTransports() {
         return this.transports.size > 0;
+    }
+    
+    public async listen() {
+        if (!this.hasTransports()) {
+            throw new Error('Missing valid IPC transport to initiate meteor-vite worker process!');
+        }
+        
+        for (const adapter of this.transports) {
+            await adapter.listen(async (message) => {
+                if (!message || !message.method) {
+                    console.error('Vite: Unrecognized worker IPC message', { message });
+                    return;
+                }
+                
+                const callWorkerMethod = IpcMethods[message.method];
+                
+                if (typeof callWorkerMethod !== 'function') {
+                    console.error(`Vite: The provided IPC method hasn't been defined yet!`, { message });
+                }
+                
+                await callWorkerMethod(...message.params as [params: any]).catch(
+                    createErrorHandler('Vite: worker process encountered an exception!')
+                )
+            })
+        }
     }
 }
 
