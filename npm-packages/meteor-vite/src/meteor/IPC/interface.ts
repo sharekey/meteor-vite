c6341dea782da7b8cb96be552c3c938c1e17f3ed
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-23 14:05:53 +0300

    Remove old IPC interface definitions

--- npm-packages/meteor-vite/src/meteor/IPC/interface.ts
+++ npm-packages/meteor-vite/src/meteor/IPC/interface.ts
@@ -1,18 +1,3 @@
-export default function CreateIPCInterface<
-    Methods extends {
-        [key in string]: (reply: IPCReply<{ readonly kind: string, data: unknown }>, ...params: [params: any]) => void;
-    },
->(methods: Methods) {
-    return methods;
-}
-
-export type IPCInterface<Methods, Replies> = { [key in keyof Methods]: (reply: (data: Replies) => void) => void }
-
-export type IPCReply<Reply extends {
-    readonly kind: string;
-    data: unknown;
-}> = (reply: Reply) => void;
-
 export function defineIpcMethods<TMethods extends {
     [key in string]: (...params: any) => Promise<void> | void;
 }>(methods: TMethods): TMethods {
