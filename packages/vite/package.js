c13c2cf41e9dae1d4f7ab9338bc5d1ae4ccbadc0
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-07 17:17:44 +0200

    Correct main module for package

--- packages/vite/package.js
+++ packages/vite/package.js
@@ -20,7 +20,8 @@ Package.onUse((api) => {
     api.versionsFrom(['3.0']);
     api.use([
         'isobuild:compiler-plugin',
+        'ecmascript'
     ]);
 
-    api.mainModule('dist/server.js', ['server'], { bare: false });
+    api.mainModule('dist/server.mjs', ['server'], { bare: false });
 });
