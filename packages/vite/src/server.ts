52182b2d884d84440350a950dd51ad2bcfc9e45f
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-07 16:35:37 +0200

    Create Vite dev server in Node VM

--- packages/vite/src/server.ts
+++ packages/vite/src/server.ts
@@ -1,1 +1,16 @@
-// todo
+import { Script, constants } from 'vm';
+
+// language=typescript
+const ViteRuntime = new Script(`
+    console.log('Preparing new Vite runtime environment');
+
+    import('vite').then(async ({ createServer }) => {
+        const server = createServer();
+        await server.listen();
+        server.printUrls();
+    })
+`, {
+    importModuleDynamically: constants.USE_MAIN_CONTEXT_DEFAULT_LOADER,
+});
+
+ViteRuntime.runInThisContext();
