59212ef48f26718ea1f7cd0d3ca3cb62e9a1c58f
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-07 16:16:21 +0200

    Add compiler package and dependencies

--- packages/vite/package.js
+++ packages/vite/package.js
@@ -3,3 +3,25 @@ Package.describe({
     version: '1.0.0',
     summary: '',
 });
+
+Package.registerBuildPlugin({
+    name: 'vite',
+    use: [],
+    sources: [
+        'plugin.js',
+    ],
+});
+
+Npm.depends({
+    vite: '6.0.0-alpha.24',
+})
+
+Package.onUse((api) => {
+    api.versionsFrom(['3.0']);
+    api.use([
+        'isobuild:compiler-plugin',
+        'typescript'
+    ]);
+
+    api.mainModule('server.js', ['server'], { bare: false });
+});
