36dc35076e9854c0ba9694678ca9abc51be42bea
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-07 17:03:53 +0200

    Explicitly set entry and out for bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+    entry: {
+      server: './packages/vite/src/server.ts'
+    },
+    outDir: './packages/vite/dist',
     skipNodeModulesBundle: true,
     target: 'es2022',
     platform: 'node',
     sourcemap: true,
+    format: 'esm',
 })
