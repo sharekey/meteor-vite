07d930b63f82e275ea2f88114bf49ad6adc2356b
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-08 21:21:12 +0200

    Clean up Meteor methods and publications before reloading server modules

--- npm-packages/meteor-vite/src/bootstrap/HMRServerCleanup.ts
+++ npm-packages/meteor-vite/src/bootstrap/HMRServerCleanup.ts
@@ -1,5 +1,20 @@
 /// <reference types="vite/client" />
+import { Meteor } from 'meteor/meteor';
+import pc from 'picocolors';
+import PackageJson from '../../package.json';
 
 if (import.meta.hot) {
-    console.log('Im responsible for cleaning up the server on HMR')
+    import.meta.hot.on('vite:beforeFullReload', () => {
+        Meteor.server.method_handlers = {};
+        Meteor.server.publish_handlers = {};
+        console.info([
+            `[${pc.cyan('HMR')}] Cleaned up Meteor method and publish handles`,
+            pc.dim([
+                'If there are other resources that persist after hot-reloading,',
+                'please open an issue over on GitHub so we can have that taken care of.',
+                `🐛 ${pc.blue(PackageJson.bugs.url)}`
+            ].join('\n'))
+        ].join('\n')
+        );
+    })
 }
