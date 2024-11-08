9102d1a324ac1ee5a7659382ae6ffcac1072dcef
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-08 14:06:37 +0200

    Resolve package.json from environment variables

--- npm-packages/meteor-vite/src/bootstrap/ViteDevServer.ts
+++ npm-packages/meteor-vite/src/bootstrap/ViteDevServer.ts
@@ -1,5 +1,7 @@
 import FS from 'node:fs';
+import Path from 'node:path';
 import { createServer, resolveConfig, type ViteDevServer } from 'vite';
+import { MeteorViteError } from '../error/MeteorViteError';
 import { meteorWorker } from '../plugin/Meteor';
 import type { ProjectJson } from '../VitePluginSettings';
 
@@ -31,7 +33,23 @@ export async function initializeViteDevServer(): Promise<ViteDevServer> {
 }
 
 function resolveContext(): { packageJson: ProjectJson } {
+    const { METEOR_PROJECT_ROOT } = process.env;
+    
+    if (!METEOR_PROJECT_ROOT) {
+        throw new MeteorViteError('Unable to determine root path for your Meteor project')
+    }
+    
+    const packageJsonPath = Path.join(METEOR_PROJECT_ROOT, 'package.json');
+    
+    if (!FS.existsSync(packageJsonPath)) {
+        throw new MeteorViteError(`Could not locate project's package.json file!`, {
+            subtitle: packageJsonPath,
+        })
+    }
+    
+    const packageJson = JSON.parse(FS.readFileSync(packageJsonPath, 'utf8'))
+    
     return {
-        packageJson: JSON.parse(FS.readFileSync('package.json', 'utf8'))
+        packageJson,
     }
 }
