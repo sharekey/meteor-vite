9adbcf28cf92d4ea7adaaceb096cbcd3b40eda93
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-09 01:19:41 +0200

    Allow disabling Vite dev server through environment variables

--- packages/vite-bundler/src/server.ts
+++ packages/vite-bundler/src/server.ts
@@ -1,5 +1,6 @@
 import HTTP from 'http';
 import { WebApp, WebAppInternals } from 'meteor/webapp';
+import { getBuildConfig } from './utility/Helpers';
 import Logger from './utility/Logger';
 import type { BoilerplateData } from './vite-boilerplate/common';
 import { ViteDevServerWorker } from './vite-boilerplate/development';
@@ -8,36 +9,43 @@ import { ViteProductionBoilerplate } from './vite-boilerplate/production';
 const worker = Meteor.isProduction ? new ViteProductionBoilerplate()
                                    : new ViteDevServerWorker();
 
-if ('start' in worker) {
-    Meteor.startup(() => {
-        worker.start();
-    })
+if (!getBuildConfig().pluginEnabled) {
+    Logger.warn('Meteor Vite plugin disabled. No Vite assets will be served to clients');
 }
 
-WebAppInternals.registerBoilerplateDataCallback('meteor-vite', async (request: HTTP.IncomingMessage, data: BoilerplateData) => {
-    const { dynamicBody, dynamicHead } = await worker.getBoilerplate();
-    
-    if (dynamicHead) {
-        data.dynamicHead = `${data.dynamicHead || ''}\n${dynamicHead}`;
+else {
+    if ('start' in worker) {
+        Meteor.startup(() => {
+            worker.start();
+        })
     }
     
-    if (dynamicBody) {
-        data.dynamicBody = `${data.dynamicBody || ''}\n${dynamicBody}`;
-    }
-});
-
-if (worker instanceof ViteProductionBoilerplate) {
-    Meteor.startup(() => {
-        Logger.debug(`Vite asset base URL: ${worker.baseUrl}`);
-        worker.makeViteAssetsCacheable();
-    })
+    WebAppInternals.registerBoilerplateDataCallback('meteor-vite', async (request: HTTP.IncomingMessage, data: BoilerplateData) => {
+        const { dynamicBody, dynamicHead } = await worker.getBoilerplate();
+        
+        if (dynamicHead) {
+            data.dynamicHead = `${data.dynamicHead || ''}\n${dynamicHead}`;
+        }
+        
+        if (dynamicBody) {
+            data.dynamicBody = `${data.dynamicBody || ''}\n${dynamicBody}`;
+        }
+    });
     
-    // Prevent Meteor from sending a 200 OK HTML file when the request is clearly not valid.
-    // If an asset is found by Meteor, this hook will not be called.
-    WebApp.connectHandlers.use(worker.assetDir, (req, res, next) => {
-        res.writeHead(404, 'Not found');
-        res.write('Vite asset could not be found.')
-        Logger.warn(`Served 404 for Vite asset request: ${req.originalUrl}`);
-        res.end();
-    })
+    if (worker instanceof ViteProductionBoilerplate) {
+        Meteor.startup(() => {
+            Logger.debug(`Vite asset base URL: ${worker.baseUrl}`);
+            worker.makeViteAssetsCacheable();
+        })
+        
+        // Prevent Meteor from sending a 200 OK HTML file when the request is clearly not valid.
+        // If an asset is found by Meteor, this hook will not be called.
+        WebApp.connectHandlers.use(worker.assetDir, (req, res, next) => {
+            res.writeHead(404, 'Not found');
+            res.write('Vite asset could not be found.')
+            Logger.warn(`Served 404 for Vite asset request: ${req.originalUrl}`);
+            res.end();
+        })
+    }
 }
+
