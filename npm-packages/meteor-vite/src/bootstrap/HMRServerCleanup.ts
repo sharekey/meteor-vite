a6b0a8972fed68d44b8572ed58327b5642e12727
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-08 21:28:51 +0200

    Log how many method handles are cleaned up

--- npm-packages/meteor-vite/src/bootstrap/HMRServerCleanup.ts
+++ npm-packages/meteor-vite/src/bootstrap/HMRServerCleanup.ts
@@ -5,10 +5,14 @@ import PackageJson from '../../package.json';
 
 if (import.meta.hot) {
     import.meta.hot.on('vite:beforeFullReload', () => {
+        const count = {
+            methodHandles: pc.yellow(Object.keys(Meteor.server.method_handlers).length.toLocaleString()),
+            publishHandlers: pc.yellow(Object.keys(Meteor.server.publish_handlers).length.toLocaleString()),
+        }
         Meteor.server.method_handlers = {};
         Meteor.server.publish_handlers = {};
         console.info([
-            `[${pc.cyan('HMR')}] Cleaned up Meteor method and publish handles`,
+            `[${pc.cyan('HMR')}] Cleaned up ${count.methodHandles} method and ${count.publishHandlers} publish handlers`,
             pc.dim([
                 'If there are other resources that persist after hot-reloading,',
                 'please open an issue over on GitHub so we can have that taken care of.',
