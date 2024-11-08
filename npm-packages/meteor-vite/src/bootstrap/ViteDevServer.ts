6e012a0b25eaa95d16f8be8f57818b80eb36d88f
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-08 14:39:14 +0200

    Use global context for retrieving packageJson

--- npm-packages/meteor-vite/src/bootstrap/ViteDevServer.ts
+++ npm-packages/meteor-vite/src/bootstrap/ViteDevServer.ts
@@ -1,9 +1,6 @@
-import FS from 'node:fs';
-import Path from 'node:path';
+import 'meteor/jorgenvatle:vite'
 import { createServer, resolveConfig, type ViteDevServer } from 'vite';
-import { MeteorViteError } from '../error/MeteorViteError';
 import { meteorWorker } from '../plugin/Meteor';
-import type { ProjectJson } from '../VitePluginSettings';
 
 /**
  * Helper function for Meteor to launch the Vite dev server within a virtual context.
@@ -11,7 +8,7 @@ import type { ProjectJson } from '../VitePluginSettings';
  * starting Meteor in a development environment.
  */
 export async function initializeViteDevServer(): Promise<ViteDevServer> {
-    const { packageJson } = resolveContext();
+    const { packageJson } = globalThis.MeteorViteRuntimeConfig;
     const config = await resolveConfig({
         configFile: packageJson.meteor.vite?.configFile
     }, 'serve');
@@ -30,26 +27,4 @@ export async function initializeViteDevServer(): Promise<ViteDevServer> {
     server.printUrls();
     
     return server;
-}
-
-function resolveContext(): { packageJson: ProjectJson } {
-    const { METEOR_PROJECT_ROOT } = process.env;
-    
-    if (!METEOR_PROJECT_ROOT) {
-        throw new MeteorViteError('Unable to determine root path for your Meteor project')
-    }
-    
-    const packageJsonPath = Path.join(METEOR_PROJECT_ROOT, 'package.json');
-    
-    if (!FS.existsSync(packageJsonPath)) {
-        throw new MeteorViteError(`Could not locate project's package.json file!`, {
-            subtitle: packageJsonPath,
-        })
-    }
-    
-    const packageJson = JSON.parse(FS.readFileSync(packageJsonPath, 'utf8'))
-    
-    return {
-        packageJson,
-    }
 }
