57f8a7d9a5b6bab1c46cc6eb09b85eac1ef71eaf
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-08 22:05:22 +0200

    Avoid removing initial publish and method handlers added by Meteor and other packages

--- npm-packages/meteor-vite/src/bootstrap/HMRServerCleanup.ts
+++ npm-packages/meteor-vite/src/bootstrap/HMRServerCleanup.ts
@@ -4,13 +4,21 @@ import pc from 'picocolors';
 import PackageJson from '../../package.json';
 
 if (import.meta.hot) {
+    // Initial handles
+    // Todo: refactor to use stubs for tracking methods and publications created by the server entry.
+    const { method_handlers, publish_handlers } = Meteor.server;
+    Meteor.startup(async () => {
+        Object.assign(method_handlers, Meteor.server.method_handlers)
+        Object.assign(publish_handlers, Meteor.server.publish_handlers);
+    });
+    
     import.meta.hot.on('vite:beforeFullReload', () => {
         const count = {
             methodHandles: pc.yellow(Object.keys(Meteor.server.method_handlers).length.toLocaleString()),
             publishHandlers: pc.yellow(Object.keys(Meteor.server.publish_handlers).length.toLocaleString()),
         }
-        Meteor.server.method_handlers = {};
-        Meteor.server.publish_handlers = {};
+        Meteor.server.method_handlers = Object.assign({}, method_handlers);
+        Meteor.server.publish_handlers = Object.assign({}, publish_handlers);
         console.info([
             `[${pc.cyan('HMR')}] Cleaned up ${count.methodHandles} method and ${count.publishHandlers} publish handlers`,
             [
