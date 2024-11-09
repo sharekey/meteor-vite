38ae3f039edd9d6bd9192a4f6c4ac2ec840e678b
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-09 15:05:29 +0200

    Add entry for build module

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     entry: {
-      server: './packages/vite/src/server.ts'
+        server: './packages/vite/src/server.ts',
+        build: './packages/vite/src/build.ts',
     },
     outDir: './packages/vite/dist',
     skipNodeModulesBundle: true,
+    splitting: false,
     target: 'es2022',
     platform: 'node',
     sourcemap: true,
