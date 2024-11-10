f7e552603155b6573c05116160fb800cc65f4726
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-10 11:20:56 +0200

    Use self-managed builder to build environments

--- npm-packages/meteor-vite/src/bootstrap/Build.ts
+++ npm-packages/meteor-vite/src/bootstrap/Build.ts
@@ -1,15 +1,13 @@
+import pc from 'picocolors';
 import type { InputOption } from 'rollup';
-import { build, version } from 'vite';
+import { createBuilder, version } from 'vite';
 import { MeteorViteError } from '../error/MeteorViteError';
-import { meteorWorker } from '../plugin/Meteor';
-import Logger from '../utilities/Logger';
-import { resolveMeteorViteConfig } from '../VitePluginSettings';
+import Logger, { BuildLogger } from '../utilities/Logger';
+import { resolveMeteorViteConfig } from './Config';
 
 export async function buildForProduction() {
     Logger.info(`Building with Vite v${version}...`);
-    const { packageJson, projectRoot } = globalThis.MeteorViteRuntimeConfig;
-    process.chdir(projectRoot);
-    const { config, buildConfig } = await resolveMeteorViteConfig({ mode: 'production' }, 'build');
+    const { config } = await resolveMeteorViteConfig({ mode: 'production' }, 'build');
     
     if (!config.meteor?.clientEntry) {
         throw new MeteorViteError('No client entrypoint specified in Vite config!')
@@ -24,25 +22,11 @@ export async function buildForProduction() {
         input.server = config.meteor.serverEntry;
     }
     
-    // Todo: refactor to using a single shared configuration, utilizing the environments
-    //  API to apply configuration specific to the client/server in a single module.
-    const result = await build({
-        base: '/vite',
-        appType: 'custom',
-        server: { middlewareMode: true, },
-        mode: 'development',
-        configFile: config.configFile,
-        plugins: [
-            meteorWorker({
-                meteorStubs: { packageJson }
-            })
-        ],
-        build: {
-            manifest: 'vite-manifest.json',
-            outDir: buildConfig.tempDir,
-            rollupOptions: { input }
-        },
-    });
+    const builder = await createBuilder(config);
     
-    // process the build
+    for (const [name, environment] of Object.entries(builder.environments)) {
+        if (name === 'ssr') continue;
+        BuildLogger.info(`Preparing ${pc.yellow(name)} bundle...`);
+        await builder.build(environment);
+    }
 }
