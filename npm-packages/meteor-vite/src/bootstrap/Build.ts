2d2751d023586c93000c8c620bb8c0ef6b8b2860
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-10 12:16:44 +0200

    Add compiler plugin

--- npm-packages/meteor-vite/src/bootstrap/Build.ts
+++ npm-packages/meteor-vite/src/bootstrap/Build.ts
@@ -1,3 +1,5 @@
+import { type BuildPluginFile, BUNDLE_FILE_EXTENSION } from 'meteor/jorgenvatle:vite-bundler/plugin/Compiler';
+import Path from 'node:path';
 import pc from 'picocolors';
 import type { InputOption } from 'rollup';
 import { createBuilder, version } from 'vite';
@@ -28,5 +30,35 @@ export async function buildForProduction() {
         if (name === 'ssr') continue;
         BuildLogger.info(`Preparing ${pc.yellow(name)} bundle...`);
         await builder.build(environment);
+    }
+}
+
+class CompilerPlugin {
+    constructor(public readonly config: { distDir: string }) {
+    }
+    processFilesForTarget(files: BuildPluginFile[]) {
+        files.forEach(file => {
+            const fileMeta = {
+                _original: {
+                    basename: file.getBasename(),
+                    path: file.getPathInPackage(),
+                },
+                basename: this._formatFilename(file.getBasename()),
+                path: this._formatFilename(file.getPathInPackage()),
+                relativePath: Path.relative(this.config.distDir, this._formatFilename(file.getPathInPackage())),
+            }
+            const sourcePath = file.getPathInPackage();
+            
+            Logger.debug(`[${file.getArch()}] Processing: ${fileMeta.basename}`, { fileMeta });
+            
+            file.addAsset({
+                path: fileMeta.relativePath,
+                data: file.getContentsAsBuffer(),
+                sourcePath,
+            });
+        })
+    }
+    protected _formatFilename(nameOrPath: string) {
+        return nameOrPath.replace(`.${BUNDLE_FILE_EXTENSION}`, '');
     }
 }
