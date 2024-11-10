8c0a94aaddb71be5db21cdcbc9eadcd7f73ec68f
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-10 09:10:58 +0200

    Only build Vite in production environments

--- packages/vite/src/build.ts
+++ packages/vite/src/build.ts
@@ -1,10 +1,13 @@
 import { runBootstrapScript } from './util/Bootstrap';
+import { CurrentConfig } from './util/CurrentConfig';
 import Logger from './util/Logger';
 
-try {
-    await runBootstrapScript('buildForProduction')
-} catch (error) {
-    Logger.error('build failed');
-    console.error(error);
-    throw error;
+if (CurrentConfig.mode === 'production') {
+    try {
+        await runBootstrapScript('buildForProduction')
+    } catch (error) {
+        Logger.error('build failed');
+        console.error(error);
+        throw error;
+    }
 }
