ff8f8bd8f9cad1d556cb9cd1f0af29f4296e3048
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-10 09:08:55 +0200

    Only start vite dev server in non-production environments

--- packages/vite/src/server.ts
+++ packages/vite/src/server.ts
@@ -1,14 +1,14 @@
 import { runBootstrapScript } from './util/Bootstrap';
 import Logger from './util/Logger';
 
-
-try {
-    await runBootstrapScript('initializeViteDevServer');
-    Logger.success('Vite should be ready to go!');
-}  catch (error) {
-    Logger.warn('Failed to start Vite dev server!');
-    console.error(error);
+if (process.env.NODE_ENV !== 'production') {
+    try {
+        await runBootstrapScript('initializeViteDevServer');
+        Logger.success('Vite should be ready to go!');
+    }  catch (error) {
+        Logger.warn('Failed to start Vite dev server!');
+        console.error(error);
+    }
 }
-
 
 export {}
