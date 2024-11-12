57d014fdbe930ccfe4dab1fbdf5bd4a3bdfa576d
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-13 01:03:32 +0200

    Use server build-time server proxy module for preparing production environment.

--- packages/vite/src/server.ts
+++ packages/vite/src/server.ts
@@ -10,26 +10,6 @@ if (CurrentConfig.mode !== 'production') {
         Logger.warn('Failed to start Vite dev server!');
         console.error(error);
     }
-} else if (Meteor.isProduction) {
-    // Todo: Maybe this could just be injected into the production server bundle.
-    try {
-        globalThis.Assets = Assets;
-        await runBootstrapScript('initializeViteProductionEnvironment');
-        Logger.success('Production environment initialized');
-    } catch (error) {
-        Logger.warn('Failed to initialize production environment!');
-        
-        /**
-         * The current (Meteor v3.0.4) release appears to swallow errors thrown by
-         * modules that utilize top-level-awaits.
-         *
-         * To be as safe as possible, we're going to forcefully terminate the Meteor
-         * process instead of throwing to avoid an unfortunate situation where a
-         * deployment monitor might report Meteor as healthy when it really is not.
-         */
-        console.error(error);
-        process.exit(1);
-    }
 }
 
 export {}
