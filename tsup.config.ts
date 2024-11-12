09c5aeeda50a7318a9978cfe8c8018a458c87d60
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-12 18:17:56 +0200

    Stub out Meteor imports to prevent conflicts with Meteor's internal modules context

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,4 +1,6 @@
+import pc from 'picocolors';
 import { defineConfig } from 'tsup';
+import type { Plugin } from 'esbuild';
 
 export default defineConfig({
     entry: {
@@ -12,4 +14,42 @@ export default defineConfig({
     platform: 'node',
     sourcemap: true,
     format: 'esm',
+    plugins: [
+        meteorImportStubs({
+            'meteor': () => 'export const Meteor = PackageStub.Meteor || globalThis.Meteor',
+            'webapp': () => 'export const WebApp = PackageStub.WebApp || globalThis.WebApp',
+        })
+    ]
 })
+
+function meteorImportStubs(packages: {
+    [key in string]: () => string;
+}): Plugin {
+    const filter = /^meteor\//;
+    return {
+        name: 'meteor-import-stubs',
+        setup(build) {
+            build.onResolve({ filter }, (args) => {
+                return { path: args.path.replace(filter, '') , namespace: 'meteor' }
+            })
+            
+            build.onLoad({ filter: /.*/, namespace: 'meteor' }, (args) => {
+                console.log(pc.cyan(`Stubbing Meteor package import: '${pc.green(args.path)}'`));
+                
+                const [packageName] = args.path.split('/');
+                const stubFunction = packages[packageName];
+                
+                if (!stubFunction) {
+                    throw new Error('Meteor package is missing stubs: ' + pc.yellow(args.path));
+                }
+                
+                return {
+                    contents: `
+                        const PackageStub = Package[${JSON.stringify(packageName)}];
+                        ${stubFunction()}
+                    `
+                }
+            })
+        }
+    }
+}
