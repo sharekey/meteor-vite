cebd71662431126b24dcdb34df072eb2d013ca2c
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-13 03:53:35 +0200

    Apply production boilerplate on server startup

--- npm-packages/meteor-vite/src/bootstrap/ProductionEnvironment.ts
+++ npm-packages/meteor-vite/src/bootstrap/ProductionEnvironment.ts
@@ -1,13 +1,15 @@
+import type * as _ from  'meteor/jorgenvatle:vite';
 import { Meteor } from 'meteor/meteor';
+import { WebAppInternals } from 'meteor/webapp';
+import { ViteProductionBoilerplate } from '../meteor/boilerplate/Production';
 
 Meteor.startup(async () => {
     console.log('[Vite] Fetching manifest...');
-    console.log(await Assets.getTextAsync('vite/client.manifest.json'));
+    const manifest = await Assets.getTextAsync('vite/client.manifest.json');
+    const boilerplate = new ViteProductionBoilerplate(JSON.parse(manifest));
     
-    
-    // Todo:
-    // Parse manifest file
-    // Prepare preload directives for entry modules
-    // Prepare lazy/low-priority preload directives to be added gradually over time.
+    WebAppInternals.registerBoilerplateDataCallback('vite', (req, data) => {
+        Object.assign(data, boilerplate.getBoilerplate());
+    });
 })
 
