96e333185c2ba4a68b16d22b91912f874a050de6
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-13 04:29:42 +0200

    Add error handling to boilerplate

--- npm-packages/meteor-vite/src/bootstrap/ProductionEnvironment.ts
+++ npm-packages/meteor-vite/src/bootstrap/ProductionEnvironment.ts
@@ -2,14 +2,29 @@ import type * as _ from  'meteor/jorgenvatle:vite';
 import { Meteor } from 'meteor/meteor';
 import { WebAppInternals } from 'meteor/webapp';
 import { ViteProductionBoilerplate } from '../meteor/boilerplate/Production';
+import type { ViteManifestFile } from '../meteor/IPC/methods/build';
 
 Meteor.startup(async () => {
     console.log('[Vite] Fetching manifest...');
     const manifest = await Assets.getTextAsync('vite/client.manifest.json');
-    const boilerplate = new ViteProductionBoilerplate(JSON.parse(manifest));
+    const files: Record<string, ViteManifestFile> = JSON.parse(manifest);
+    const boilerplate = new ViteProductionBoilerplate({
+        base: 'vite',
+        assetsDir: 'vite-assets',
+        files,
+    });
     
-    WebAppInternals.registerBoilerplateDataCallback('vite', (req, data) => {
-        Object.assign(data, boilerplate.getBoilerplate());
+    console.log(boilerplate.viteManifest);
+    
+    WebAppInternals.registerBoilerplateDataCallback('vite', async (req, data) => {
+        try {
+            const { dynamicHead, dynamicBody } = boilerplate.getBoilerplate();
+            
+            console.log({ dynamicBody, dynamicHead });
+        } catch (error) {
+            console.warn(error);
+            throw error;
+        }
     });
 })
 
