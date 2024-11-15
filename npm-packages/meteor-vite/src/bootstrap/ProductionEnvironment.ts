05ec77efe51585a30509c95bd6173f95aa41d5ac
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-15 20:01:37 +0200

    Correct default assets dir

--- npm-packages/meteor-vite/src/bootstrap/ProductionEnvironment.ts
+++ npm-packages/meteor-vite/src/bootstrap/ProductionEnvironment.ts
@@ -9,9 +9,11 @@ Meteor.startup(async () => {
     console.log('[Vite] Fetching manifest...');
     const manifest = await Assets.getTextAsync('vite/client.manifest.json');
     const files: Record<string, ViteManifestFile> = JSON.parse(manifest);
+    
+    // Todo: retrieve base and assets dir from build config/manifest file
     const boilerplate = new ViteProductionBoilerplate({
         base: 'vite',
-        assetsDir: 'vite-assets',
+        assetsDir: 'vite',
         files,
     });
     
@@ -36,7 +38,7 @@ Meteor.startup(async () => {
         }
     });
     
-    WebApp.handlers.use(boilerplate.baseUrl, (req, res, next) => {
+    WebApp.connectHandlers.use(boilerplate.baseUrl, (req, res, next) => {
         res.writeHead(404, 'Not found');
         res.write('Vite asset could not be found');
         Logger.warn(`Served 404 for unknown Vite asset: ${req.originalUrl}`);
