59a170b9fbef9deea93759816ce1cc87cf0849dd
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-15 21:44:14 +0200

    Always run project setup with build plugin

--- packages/vite/src/build.ts
+++ packages/vite/src/build.ts
@@ -56,6 +56,9 @@ class CompilerPlugin {
     }
 }
 
+// Cleanup temporary files from previous builds.
+await runBootstrapScript('setupProject');
+
 if (CurrentConfig.mode === 'production') {
     try {
         const bundle = runBootstrapScript('buildForProduction');
