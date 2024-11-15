2712c37a37c18f208142f3202e8e22d863dd275f
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-15 21:27:22 +0200

    Add cleanup script to setup

--- npm-packages/meteor-vite/src/bootstrap/Setup.ts
+++ npm-packages/meteor-vite/src/bootstrap/Setup.ts
@@ -1,4 +1,20 @@
+import FS from 'node:fs';
+import { createSimpleLogger } from '../utilities/Logger';
+import pc from 'picocolors';
+import { CurrentConfig } from './Config';
+
+const logger = createSimpleLogger('Setup');
+
 export function setupProject() {
-    // Cleanup previous build output
+    cleanupPreviousBuilds();
     // Create entry modules for the server.
+}
+
+/**
+ * Clean up temporary files created by previous production builds.
+ * Remaining build files can interfere with the dev server
+ */
+function cleanupPreviousBuilds() {
+    FS.rmSync(CurrentConfig.outDir, { recursive: true, force: true });
+    logger.debug(`Cleaned up old build output in ${pc.green(CurrentConfig.outDir)}`);
 }
