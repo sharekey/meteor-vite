bfa28f63cb7a836688d5ff7fa84193dd28a2d3b8
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-15 21:40:28 +0200

    Move entry module creation into setup

--- npm-packages/meteor-vite/src/bootstrap/Config.ts
+++ npm-packages/meteor-vite/src/bootstrap/Config.ts
@@ -1,6 +1,5 @@
 import FS from 'fs';
 import Path from 'path';
-import pc from 'picocolors';
 import {
     type BuildEnvironmentOptions,
     createNodeDevEnvironment,
@@ -10,10 +9,9 @@ import {
 } from 'vite';
 import { MeteorViteError } from '../error/MeteorViteError';
 import { meteorWorker } from '../plugin/Meteor';
-import { homepage } from '../utilities/Constants';
-import Logger from '../utilities/Logger';
 import { type ProjectJson, type ResolvedMeteorViteConfig } from '../VitePluginSettings';
 import Instance from './Instance';
+import { serverRollupInput } from './Setup';
 
 export const CurrentConfig = globalThis.MeteorViteRuntimeConfig;
 
@@ -31,10 +29,7 @@ export async function resolveMeteorViteConfig(
     let serverBuildConfig: BuildEnvironmentOptions | undefined = undefined;
     const outDir = Path.join(CurrentConfig.tempDir, 'dist');
     
-    prepareServerEntry();
-    
     if (userConfig.meteor?.serverEntry) {
-        injectServerEntryImport(packageJson.meteor.mainModule.server);
         serverBuildConfig = {
             target: 'node20',
             manifest: false,
@@ -43,7 +38,10 @@ export async function resolveMeteorViteConfig(
             sourcemap: true,
             rollupOptions: {
                 input: {
-                    main: prepareProductionServerProxyModule(userConfig.meteor.serverEntry),
+                    main: serverRollupInput({
+                        meteorMainModule: packageJson.meteor.mainModule.server,
+                        viteServerEntry: userConfig.meteor.serverEntry,
+                    }),
                 },
                 output: {
                     // Unfortunately Meteor still doesn't support
@@ -128,67 +126,6 @@ export async function resolveMeteorViteConfig(
         packageJson,
         outDir,
     }
-}
-
-/**
- * Create an empty entry module that can imported by Meteor's mainModule configured in package.json.
- */
-function prepareServerEntry() {
-    FS.mkdirSync(Path.dirname(CurrentConfig.serverEntryModule), { recursive: true });
-    FS.writeFileSync(
-        Path.join(
-            Path.dirname(CurrentConfig.serverEntryModule),
-            '.gitignore'
-        ),
-        '*'
-    );
-    FS.writeFileSync(CurrentConfig.serverEntryModule, '// Dynamic entrypoint for the Meteor server. Imports are added here during builds');
-}
-
-/**
- * Add an import for the Vite-built server entry module to Meteor's configured mainModule.
- * This ensures that assets built by Vite will actually be loaded by the Meteor server after
- * creating a production build. Otherwise, the files emitted by Vite will be ignored by the
- * Meteor server.
- */
-function injectServerEntryImport(mainModule: string | undefined) {
-    if (!mainModule) {
-        throw new MeteorViteError('Could not find a server mainModule path in your package.json!', {
-            subtitle: `Visit ${pc.blue(homepage)} for more details`
-        })
-    }
-    
-    const originalContent = FS.readFileSync(mainModule, 'utf-8');
-    const importPath = Path.relative(Path.dirname(mainModule), CurrentConfig.serverEntryModule);
-    
-    if (originalContent.includes(importPath)) {
-        return;
-    }
-    
-    Logger.warn(`Meteor-Vite needs to write to the Meteor main module defined in your package.json`);
-    Logger.warn(`If you've migrated an existing project, please make sure to move any existing code in this file over to the entry module specified in your Vite config.`);
-    
-    
-    FS.writeFileSync(mainModule, [
-        `/**`,
-        ` * These modules are automatically imported by jorgenvatle:vite-bundler.`,
-        ` * You can commit these to your project or move them elsewhere if you'd like,`,
-        ` * but they must be imported somewhere in your Meteor mainModule.`,
-        ` *`,
-        ` * More info: https://github.com/JorgenVatle/meteor-vite#lazy-loaded-meteor-packages`,
-        ` **/`,
-        `import ${JSON.stringify(importPath)}`,
-        '/** End of vite-bundler auto-imports **/',
-        originalContent,
-    ].join('\n'));
-}
-
-function prepareProductionServerProxyModule(serverEntry: string) {
-    FS.writeFileSync(CurrentConfig.serverProductionProxyModule, [
-        `import "meteor-vite/bootstrap/ProductionEnvironment"`,
-        `import ${JSON.stringify(Path.resolve(CurrentConfig.projectRoot, serverEntry))}`,
-    ].join('\n'));
-    return CurrentConfig.serverProductionProxyModule;
 }
 
 function parsePackageJson(): ProjectJson {
