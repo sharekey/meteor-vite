ff63eeb905506d7026c2b41d734321f99f5fe211
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-18 20:41:20 +0200

    Fix stub for Plugin global from isobuild

--- tsup.config.ts
+++ tsup.config.ts
@@ -25,7 +25,7 @@ export default defineConfig(() => ({
 
 export const EsbuildPluginMeteorStubs = meteorImportStubs({
     'meteor': () => 'export const Meteor = PackageStub.Meteor || globalThis.Meteor',
-    'isobuild': () => `export const Plugin = globalThis.Plugin`,
+    'isobuild': () => `const PluginGlobal = Plugin; export { PluginGlobal as Plugin }`,
     'webapp': () => [
         'export const WebApp = PackageStub.WebApp || globalThis.WebApp',
         'export const WebAppInternals = PackageStub.WebAppInternals || globalThis.WebAppInternals',
