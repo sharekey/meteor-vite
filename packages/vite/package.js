64afc1726d346f83d5b184ced2a843f9798a3c3f
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-18 21:39:01 +0200

    Include pre-release tag in version

--- packages/vite/package.js
+++ packages/vite/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'jorgenvatle:vite',
-    version: '1.0.0',
+    version: '1.0.0-alpha.0',
     summary: `⚡ Speeds up Meteor's build and HMR processes using Vite`,
     documentation: 'README.md'
 });
