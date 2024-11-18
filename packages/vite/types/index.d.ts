5fc794661dc4e37a632835cafb59b710beb54b05
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-18 20:33:35 +0200

    Add path reference for isobuild

--- packages/vite/types/index.d.ts
+++ packages/vite/types/index.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="./meteor/webapp.d.ts" />
 /// <reference path="./meteor/plugin.d.ts" />
 /// <reference path="./meteor/global.d.ts" />
+/// <reference path="./meteor/isobuild.d.ts" />
