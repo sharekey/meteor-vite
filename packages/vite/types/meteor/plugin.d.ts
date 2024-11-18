6dcf8cd24f1523c01a59905da522428ae31635a2
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-18 20:20:34 +0200

    Add InputFile and FileData type declarations

--- packages/vite/types/meteor/plugin.d.ts
+++ packages/vite/types/meteor/plugin.d.ts
@@ -5,6 +5,23 @@ namespace Plugin {
     }
     type FactoryFunction = () => { processFilesForTarget(): void };
     function registerCompiler(config: CompilerPluginConfig, compilerFactory: FactoryFunction): void;
+    interface InputFile {
+        getContentsAsString(): string;
+        getPathInPackage(): string;
+        getContentsAsBuffer(): PluginFileBuffer;
+        getBasename(): string;
+        addAsset(data: FileData): void;
+        addStylesheet(data: FileData): void;
+        addJavaScript(data: FileData): void;
+        getArch(): string;
+        cacheable?: boolean;
+    }
+    interface FileData {
+        path: string;
+        data: string | PluginFileBuffer;
+        sourcePath?: string;
+        sourceMap?: null | string;
+    }
 }
 
 declare global {
