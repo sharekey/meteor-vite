6daf43dbd8a15e458af1810b1e38475eb3fc053c
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-25 04:03:50 +0200

    Move internal bootstrapper to entry directory

--- npm-packages/meteor-vite/src/bootstrap/index.ts
+++
@@ -1,3 +0,0 @@
-export { initializeViteDevServer } from './ViteDevServer';
-export { buildForProduction } from './Build';
-export { setupProject } from './Setup';
---
+++ npm-packages/meteor-vite/src/entry/internal-bootstrap.ts
@@ -0,0 +1,3 @@
+export { initializeViteDevServer } from '../bootstrap/ViteDevServer';
+export { buildForProduction } from '../bootstrap/Build';
+export { setupProject } from '../bootstrap/Setup';
