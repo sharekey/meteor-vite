f18e6955630b7196cd5d87c8d83afb05204a4648
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-25 04:13:04 +0200

    Move bootstrap index module into scripts subdir

--- npm-packages/meteor-vite/src/entry/bootstrap/index.ts
+++
@@ -1,3 +0,0 @@
-export { initializeViteDevServer } from '../../bootstrap/ViteDevServer';
-export { buildForProduction } from '../../bootstrap/Build';
-export { setupProject } from '../../bootstrap/Setup';
