06f072b82ff997fabd28d200160677bc78d117c2
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-25 06:43:22 +0200

    Rename meteor-v3-vue example dir to vue-zodern-relay

--- examples/vue-zodern-relay/tests/main.js
+++ examples/vue-zodern-relay/tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("meteor-v3-vue", function () {
+describe("vue-zodern-relay", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "meteor-v3-vue");
+    assert.strictEqual(name, "vue-zodern-relay");
   });
 
   if (Meteor.isClient) {
