2e6a8fb8cb10a8b90de37281d5b72abc76a189f3
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-25 02:35:28 +0200

    Use Meteor startup block for initializing Vite dev server

--- packages/vite/src/server.ts
+++ packages/vite/src/server.ts
@@ -1,15 +1,21 @@
+import { Meteor } from 'meteor/meteor';
 import { runBootstrapScript } from './util/Bootstrap';
 import { CurrentConfig } from './util/CurrentConfig';
 import Logger from './util/Logger';
 
-if (CurrentConfig.mode !== 'production') {
+Meteor.startup(async () => {
+    if (CurrentConfig.mode === 'production') {
+        return;
+    }
+    
     try {
         await runBootstrapScript('initializeViteDevServer');
         Logger.success('Vite should be ready to go!');
     }  catch (error) {
         Logger.warn('Failed to start Vite dev server!');
         console.error(error);
+        throw error;
     }
-}
+})
 
 export {}
