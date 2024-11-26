4d6c6c6040b6a9a4b60e5e2e78467dca926add9d
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-26 17:54:54 +0200

    Add colorization helpers to use consistent colors for highlighted log items

--- npm-packages/meteor-vite/src/utilities/Constants.ts
+++ npm-packages/meteor-vite/src/utilities/Constants.ts
@@ -1,5 +1,13 @@
+import pc from 'picocolors';
 import { readFileSync } from 'node:fs';
 
 export const { version, homepage } = JSON.parse(
     readFileSync(new URL('../package.json', import.meta.url), 'utf8'),
 )
+
+export const Colorize = {
+    filepath: pc.cyan,
+    arch: pc.yellow,
+    versionNumber: pc.blue,
+    textSnippet: pc.white,
+}
