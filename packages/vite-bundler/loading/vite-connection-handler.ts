e3bc2d641eec20c224044f3db4e830bde66f0bf3
Jørgen Vatle <jorgen@wcaservices.net>
2024-12-01 20:35:13 +0200

    Add proxy module for vite-connection-handler to warn users with old React preamble code in their apps.

---
+++ packages/vite-bundler/loading/vite-connection-handler.ts
@@ -0,0 +1,19 @@
+// Old React example apps would directly import a module at this path to retrieve Vite server information
+// to then use for injecting a React preamble into the HTML served by Meteor in development mode.
+//
+// The React preamble is now automatically included by Meteor-Vite and importing anything from here
+// is no longer necessary.
+
+import { MeteorViteError } from '../src/utility/Errors';
+import pc from 'picocolors';
+
+export function getConfig() {
+    throw new MeteorViteError([
+       'Module has moved:' + pc.yellow('jorgenvatle:vite-bundler/loading/vite-connection-handler'),
+       '',
+       `You're likely seeing this because your app is generating a React preamble for your Vite dev server`,
+       'This is no longer needed and you can safely remove the boilerplate used to achieve this.',
+       '',
+       'Remove the following module if it exists in your app: https://github.com/JorgenVatle/meteor-vite/blob/d3633cb015206cb61168fa135c33b89331afeb04/examples/react/server/react-refresh.js'
+    ].join('\n'))
+}
