a40802ca6b9d9126fe6c08dcc7abf25bbc722bbf
Jørgen Vatle <jorgen@wcaservices.net>
2024-12-04 13:30:01 +0200

    Move _meteorVite type declaration into globalThis namespace

--- packages/vite/src/index.d.ts
+++ packages/vite/src/index.d.ts
@@ -7,9 +7,6 @@ declare global {
     
     namespace globalThis {
         var MeteorViteRuntimeConfig: (typeof CurrentConfig) & MeteorViteRuntimeConfig;
-    }
-    
-    namespace global {
         var _meteorVite: undefined | {
             name: string;
             version: string;
