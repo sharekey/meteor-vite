633d967b7542fb1865c5db10a896022d01ac6a91
Jørgen Vatle <jorgen@wcaservices.net>
2025-01-21 03:18:53 +0200

    Add utility function for formatting import paths to a Node-compatible format.

--- npm-packages/meteor-vite/src/utilities/Formatting.ts
+++ npm-packages/meteor-vite/src/utilities/Formatting.ts
@@ -1,3 +1,4 @@
+import Path from 'path';
 import pc from 'picocolors';
 
 export const Colorize = {
@@ -6,3 +7,13 @@ export const Colorize = {
     versionNumber: pc.blue,
     textSnippet: pc.white,
 };
+
+/**
+ * Format import paths to a Node-compatible format.
+ * Fixes issues where paths built in Windows environments are using backslashes instead of the expected forward slash
+ * format.
+ * @param path File/import path
+ */
+export function formatImportPath(path: string) {
+    return Path.posix.join(...path.split(Path.sep));
+}
