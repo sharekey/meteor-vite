ea019997893ab8f65247971494155de2204e8661
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-11 16:40:53 +0200

    Move boilerplate setup into setup scripts

--- npm-packages/meteor-vite/src/entry/bootstrap/scripts/Setup.ts
+++ npm-packages/meteor-vite/src/entry/bootstrap/scripts/Setup.ts
@@ -1,3 +1,5 @@
+import { Meteor } from 'meteor/meteor';
+import { onPageLoad } from 'meteor/server-render';
 import FS from 'node:fs';
 import Path from 'node:path';
 import type { ModulePreloadOptions } from 'vite';
@@ -9,6 +11,7 @@ import pc from 'picocolors';
 import { CurrentConfig } from '../lib/Config';
 import { satisfies, parse } from 'semver';
 import { version as npmPackageVersion } from '../../../utilities/Constants';
+import { getBoilerplate } from '../lib/RuntimeConfig';
 
 const logger = createSimpleLogger('Setup');
 
@@ -17,6 +20,26 @@ export function setupProject() {
     cleanupPreviousBuilds();
     prepareServerEntry();
     // Create entry modules for the server.
+}
+
+export function setupBoilerplate() {
+    Meteor.startup(async () => {
+        // We can rely on the Meteor WebApp for server rendering in browser contexts.
+        if (!Meteor.isCordova && !Meteor.isServer) {
+            return;
+        }
+        
+        onPageLoad(async (sink) => {
+            const { head, body } = await getBoilerplate();
+            
+            if (head) {
+                sink.appendToHead(head.join('\n'));
+            }
+            if (body) {
+                sink.appendToBody(body.join('\n'));
+            }
+        })
+    })
 }
 
 function validateVersions() {
