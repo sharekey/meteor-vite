b4641790cf507a4d93571afc1281a2d56264565e
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-11 16:11:23 +0200

    Add common environment module for tracking scripts to render to clients

---
+++ npm-packages/meteor-vite/src/entry/bootstrap/CommonEnvironment.ts
@@ -0,0 +1,26 @@
+import { Meteor } from 'meteor/meteor';
+import { onPageLoad } from 'meteor/server-render';
+
+export const RuntimeConfigCollection = new Mongo.Collection<{
+    _id: 'scripts',
+    scripts: string[];
+}>('_meteor-vite', {
+    connection: null,
+});
+
+Meteor.startup(async () => {
+    // We can rely on the Meteor WebApp for server rendering in browser contexts.
+    if (!Meteor.isCordova && !Meteor.isServer) {
+        return;
+    }
+    
+    onPageLoad(async (sink) => {
+        const document = await RuntimeConfigCollection.findOneAsync({
+            _id: 'scripts',
+        });
+        if (!document) {
+            throw new Meteor.Error(404, 'Failed to load Meteor-Vite client scripts!');
+        }
+        sink.appendToHead(document.scripts);
+    })
+})
