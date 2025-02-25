53b313efb4d9e43ec0583912142945c5ce03b96e
Jørgen Vatle <jorgen@wcaservices.net>
2025-02-25 05:57:01 +0200

    Add Vite development boilerplate

---
+++ npm-packages/meteor-vite/src/entry/bootstrap/boilerplate/Development.ts
@@ -0,0 +1,53 @@
+import { Meteor } from 'meteor/meteor';
+import Path from 'path';
+import { type Boilerplate, ViteBoilerplate } from './Boilerplate';
+
+export class ViteDevelopmentBoilerplate extends ViteBoilerplate {
+    public readonly baseUrl: string;
+    public readonly needsReactPreamble: boolean;
+    public readonly clientEntry: string;
+    
+    constructor({ clientEntry, needsReactPreamble, baseUrl }: Pick<ViteDevelopmentBoilerplate, 'baseUrl' | 'needsReactPreamble' | 'clientEntry'>) {
+        super();
+        this.clientEntry = clientEntry;
+        this.needsReactPreamble = needsReactPreamble;
+        this.baseUrl = baseUrl;
+        
+        if (!baseUrl) {
+            throw new Error('Unable to resolve base URL for Vite assets! Make sure you are importing meteor-vite as a plugin your Vite config');
+        }
+    }
+    
+    public getBoilerplate(): Promise<Boilerplate> | Boilerplate {
+        const scripts = [
+            Path.join(this.baseUrl, '@vite/client'),
+            Path.join(this.baseUrl, this.clientEntry)
+        ].map((url) => {
+            let absoluteUrl = url.replaceAll(Path.win32.sep, Path.posix.sep);
+            
+            if (!absoluteUrl.match(/https?:/)) {
+                absoluteUrl = Meteor.absoluteUrl(url)
+            }
+            
+            return `<script src="${absoluteUrl}" type="module" crossorigin></script>`;
+        });
+        
+        // ⚡ [Client/React] Add React HMR preamble
+        if (this.needsReactPreamble) {
+            scripts.unshift(`
+                <script type="module">
+                    import RefreshRuntime from "${Meteor.absoluteUrl(Path.join(this.baseUrl, '@react-refresh'))}"
+                    RefreshRuntime.injectIntoGlobalHook(window)
+                    window.$RefreshReg$ = () => {}
+                    window.$RefreshSig$ = () => (type) => type
+                    window.__vite_plugin_react_preamble_installed__ = true
+                </script>
+            `)
+        }
+        
+        return {
+            dynamicHead: scripts.join('\n'),
+            dynamicBody: '',
+        }
+    }
+}
