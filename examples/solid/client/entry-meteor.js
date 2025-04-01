f5d7997f0ceb0c86a78745a7175858137ec15238
Jørgen Vatle <Jorgen@WcAServices.net>
2025-04-01 14:01:22 +0300

    Correct path reference for Vite entrypoint

--- examples/solid/client/entry-meteor.js
+++ examples/solid/client/entry-meteor.js
@@ -2,4 +2,4 @@
 // This file would normally not contain anything but imports for Meteor packages to help with lazy loading.
 // But if you are seeing issues  with Vite, this is a good place to put any code that you don't want Vite to process.
 
-// ./vite.js is your new primary entrypoint for the Meteor client.
+// ./entry-vite.js is your new primary entrypoint for the Meteor client.
