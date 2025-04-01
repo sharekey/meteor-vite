9ed3eb6e453ed44be63f5578d629187b8e264888
Jørgen Vatle <Jorgen@WcAServices.net>
2025-04-01 20:29:26 +0300

    Correct define option key for dynamic Vite assets

--- npm-packages/meteor-vite/src/entry/plugin/Meteor.ts
+++ npm-packages/meteor-vite/src/entry/plugin/Meteor.ts
@@ -90,7 +90,7 @@ export function meteorWorker(config: PartialPluginOptions): PluginOption {
                     base,
                     define: {
                         __VITE_ASSETS_DIR__: JSON.stringify(pluginSettings.assetsDir),
-                        __VITE_STATIC_ASSET_BOILERPLATE__: JSON.stringify(pluginSettings.dynamicAssetBoilerplate),
+                        __VITE_DYNAMIC_ASSET_BOILERPLATE__: JSON.stringify(pluginSettings.dynamicAssetBoilerplate),
                     },
                     optimizeDeps: {
                         entries: [pluginSettings.clientEntry]
