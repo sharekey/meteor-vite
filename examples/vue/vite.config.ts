50a831ccefb3701d68b504589d629979162031e6
Jørgen Vatle <Jorgen@WcAServices.net>
2025-04-01 13:32:56 +0300

    Rename Vue example entry modules to match module naming scheme recommended by the readme.

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
 import { meteor } from 'meteor-vite/plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
     vue(),
     meteor({
-      clientEntry: 'imports/ui/main.ts',
-      serverEntry: 'server/main.ts',
+      clientEntry: 'client/entry-vite.ts',
+      serverEntry: 'server/entry-vite.ts',
       enableExperimentalFeatures: true,
       externalizeNpmPackages: ['test-externalization'],
       stubValidation: {
