dbd2b9ac421a11c3f13d0ac27f352353e9744f54
Jørgen Vatle <Jorgen@WcAServices.net>
2025-04-01 14:04:29 +0300

    Move contents of main.ts into Vite entry module

--- examples/.example-template/server/entry-vite.ts
+++ examples/.example-template/server/entry-vite.ts
@@ -1,6 +1,19 @@
+import { Meteor } from 'meteor/meteor';
+import { onPageLoad } from 'meteor/server-render';
+
 // Register publications and methods
 import '../imports/api/links/links.methods';
 import '../imports/api/links/links.publications';
 
-// Import server main module
-import './main';
+Meteor.startup(() => {
+    // Code to run on server startup.
+    console.log(`Greetings from ${module.id}!`);
+});
+
+onPageLoad(sink => {
+    // Code to run on every request.
+    sink.renderIntoElementById(
+        "server-render-target",
+        `Server time: ${new Date}`
+    );
+});
