92f33b6e423d4d9fb0613e130d3843589f9bd04b
Jørgen Vatle <Jorgen@WcAServices.net>
2025-04-01 21:32:17 +0300

    Remove MongoDB connection polling from startup script

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -6,9 +6,6 @@ cd $SCRIPTS_FOLDER
 MONGO_URL=$(echo "$MONGO_URL" | MONGO_DATABASE="$APP_BASENAME" envsubst)
 export MONGO_URL
 
-# Poll until we can successfully connect to MongoDB
-source ./connect-to-mongo.sh
-
 echo 'Starting app...'
 cd $APP_BUNDLE_FOLDER/bundle
 
