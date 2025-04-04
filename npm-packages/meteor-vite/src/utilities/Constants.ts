bdf90cc31d1d9a666de6d7757102bd012fea5a9b
Jørgen Vatle <jorgen@wcaservices.net>
2025-04-04 18:54:17 +0300

    Use re-export instead of file read for constants

--- npm-packages/meteor-vite/src/utilities/Constants.ts
+++ npm-packages/meteor-vite/src/utilities/Constants.ts
@@ -1,12 +1,1 @@
-import { readFileSync } from 'node:fs';
-
-export const { version, homepage, bugs } = JSON.parse(
-    readFileSync(new URL('../package.json', import.meta.url), 'utf8'),
-) as {
-    version: string;
-    homepage: string;
-    bugs: {
-        url: string;
-    };
-}
-
+export { version, bugs, homepage } from '../../package.json'
