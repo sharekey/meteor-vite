360c92f598d7b05c3c146dd874818a381d01bd98
Jørgen Vatle <jorgen@wcaservices.net>
2025-04-04 18:51:58 +0300

    Add bugs and type declaration to package.json constants

--- npm-packages/meteor-vite/src/utilities/Constants.ts
+++ npm-packages/meteor-vite/src/utilities/Constants.ts
@@ -1,6 +1,12 @@
 import { readFileSync } from 'node:fs';
 
-export const { version, homepage } = JSON.parse(
+export const { version, homepage, bugs } = JSON.parse(
     readFileSync(new URL('../package.json', import.meta.url), 'utf8'),
-)
+) as {
+    version: string;
+    homepage: string;
+    bugs: {
+        url: string;
+    };
+}
 
